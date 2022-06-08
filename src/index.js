2bc92a6088660557a470b62ec50aa024abaa600c
beakerandjake <beakerandjake@users.noreply.github.com>
2022-06-08 15:34:27 -0700

    simple tests of http methods

--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
 'use strict';
-
 import express from 'express';
+import bodyParser from 'body-parser';
+import { v4 as uuid } from 'uuid';
 
-// Constants
-const PORT = process.env.PORT || 3000;
-
-// App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
+
+
+let items = [
+  { id: "123", name: "baboon" },
+  { id: "456", name: "beaker" },
+  { id: "789", name: "coyote" },
+];
+
+app.get('/api/v1/items', (req, res) => {
+  res.send(items);
 });
 
-app.listen(PORT, () => {
-  console.log(`listo running on http://localhost:${PORT}`);
+app.post('/api/v1/items', (req, res) => {
+  const item = {
+    id: uuid(),
+    name: req.body.name,
+  };
+
+  if (items.find(x => x.name == item.name)) {
+    res.sendStatus(409);
+    return;
+  }
+
+  items.push(item);
+
+  res.sendStatus(201);
+});
+
+app.delete('/api/v1/items/:itemId', (req, res) => {
+  const index = items.findIndex(x => x.id == req.params.itemId);
+
+  if (index > -1) {
+    items.splice(index, 1);
+    res.sendStatus(200);
+    return;
+  }
+
+  res.sendStatus(404);
+});
+
+app.listen(port, () => {
+  console.log(`listo running on http://localhost:${port}`);
 });
