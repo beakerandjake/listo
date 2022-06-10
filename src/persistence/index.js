c7b8e152ea1e4ac387c1aad5296d9c6f2640ed50
beakerandjake <beakerandjake@users.noreply.github.com>
2022-06-10 15:40:43 -0700

    wip sqlite

--- src/persistence/index.js
+++ src/persistence/index.js
@@ -1,15 +1,14 @@
 // TODO switch based on config.
-let backing_store;
+let backingStore;
 
 switch (process.env.BACKING_STORE) {
-    case 'sqlite':
-        console.log('sqlite');
-        backing_store = await import('./sqlite.js');
-        break;
-    default:
-        console.log('memory');
-        backing_store = await import('./memory.js');
-        break;
+  case 'sqlite':
+    backingStore = await import('./sqlite.js');
+    break;
+  default:
+    backingStore = await import('./memory.js');
+    break;
 }
 
-export default backing_store;
+const toExport = backingStore;
+export default toExport;
--- src/persistence/sqlite.js
+++ src/persistence/sqlite.js
@@ -1,11 +1,21 @@
-// import sqlite3 from 'sqlite3';
+import fs from 'fs';
+import path from 'path';
+import sqlite3 from 'sqlite3';
 
-// sqlite3.verbose();
-// const db = new sqlite3.Database();
+const dbLocation = path.join(process.env.HOME, '.listo/items.db');
 
-// db.serialize(() => {
-//   db.run('CREATE TABLE IF NOT EXISTS hello (message TEXT)');
-// });
+// ensure db file exists.
+const dirName = path.dirname(dbLocation);
+if (!fs.existsSync(dirName)) {
+  fs.mkdirSync(dirName, { recursive: true });
+}
+
+// scaffold the db.
+sqlite3.verbose();
+const db = new sqlite3.Database(dbLocation, (err) => {
+  if (err) return;
+  db.run('CREATE TABLE IF NOT EXISTS items (id varchar(36), name TEXT)');
+});
 
 export function getAll() {
   console.log('not implemented');
