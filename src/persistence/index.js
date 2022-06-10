6f42dfa3ebd4f82e27adcd5fe7d0d61b613ce3fd
beakerandjake <beakerandjake@users.noreply.github.com>
2022-06-10 14:46:26 -0700

    dynamic backing store

--- src/persistence/index.js
+++ src/persistence/index.js
@@ -1,2 +1,15 @@
 // TODO switch based on config.
-export { add, remove, getAll } from './sqlite.js';
+let backing_store;
+
+switch (process.env.BACKING_STORE) {
+    case 'sqlite':
+        console.log('sqlite');
+        backing_store = await import('./sqlite.js');
+        break;
+    default:
+        console.log('memory');
+        backing_store = await import('./memory.js');
+        break;
+}
+
+export default backing_store;
