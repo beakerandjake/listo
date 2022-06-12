e6685d6f6869ea56ab061da8f39113240ff7dc98
beakerandjake <beakerandjake@users.noreply.github.com>
2022-06-12 14:09:46 -0700

    remove memory store

--- src/persistence/index.js
+++ src/persistence/index.js
@@ -1,12 +1,9 @@
-// TODO switch based on config.
 let backingStore;
 
+// add support for more data stores here.
 switch (process.env.BACKING_STORE) {
-  case 'sqlite':
-    backingStore = await import('./sqlite.js');
-    break;
   default:
-    backingStore = await import('./memory.js');
+    backingStore = await import('./sqlite.js');
     break;
 }
 
