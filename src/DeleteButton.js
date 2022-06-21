00f6c816bb4703d5d1de1ae699926d6491f967e2
beakerandjake <beakerandjake@users.noreply.github.com>
2022-06-20 17:42:31 -0700

    delete button hover

--- src/DeleteButton.js
+++ src/DeleteButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function DeleteButton(props) {
-    return <button className="bg-none border-none p-0 underline cursor-pointer" onClick={props.onClick} disabled={props.disabled}>Delete</button>
+    return <button className="bg-none border-none p-0 underline cursor-pointer text-gray-600 hover:text-black" onClick={props.onClick} disabled={props.disabled}>Delete</button>
 }
