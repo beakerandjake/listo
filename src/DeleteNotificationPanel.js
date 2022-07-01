409ab2e522a3cb50af157b2c0f56d534d4b2080f
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-01 16:19:43 -0700

    wip notification panel

---
+++ src/DeleteNotificationPanel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import DeleteNotification from './DeleteNotification';
+
+export default function DeleteNotificationPanel(props) {
+
+    return (
+        <div className="fixed inset-0 flex items-end px-4 py-6 sm:p-6 pointer-events-none">
+            <div className="w-full flex flex-col items-center space-y-4">
+                {props.lastDeleted &&
+                    <DeleteNotification
+                        name={props.lastDeleted.name}
+                        onUndo={() => console.log('undo')}
+                        onDismiss={() => console.log('dismiss')}
+                    />}
+            </div>
+        </div>
+    );
+}
