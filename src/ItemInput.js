62a4cc794cb6a945a8a942b54915755400a10a90
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-01 12:40:53 -0700

    fix issues with input

--- src/ItemInput.js
+++ src/ItemInput.js
@@ -1,34 +1,34 @@
 import React, { useState } from 'react';
 
 export default function ItemInput(props) {
-    const [name, setName] = useState('');
+    const [itemName, setItemName] = useState('');
 
-    const onClickAddButton = () => {
-        if (!name || name.length <= 1) {
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if (!itemName || itemName.length <= 1 || props.disabled) {
             return;
         }
 
-        props.onAddItem(name);
-        setName('');
-    };
+        props.onAddItem(itemName);
+        setItemName('');
+    }
 
-    const onKeyDown = e => {
-        if (e.key !== 'Enter') {
-            return;
-        }
-
-        onClickAddButton();
+    const onInput = e => {
+        setItemName(e.target.value);
     }
 
     return (
-        <div className="w-full pt-6 mb-4 flex items-center">
+        <form className="w-full pt-6 mb-4 flex items-center" onSubmit={handleSubmit}>
             <input
-                className="shadow appearance-none border w-full py-2 px-3 text-gray-700 focus:border-green-700 focus:outline-none" type="text"
+                className="shadow appearance-none border w-full py-2 px-3 text-gray-700 focus:outline-none"
+                type="text"
                 placeholder='What do we need?'
-                value={name}
-                onChange={e => setName(e.target.value)} onKeyDown={onKeyDown} disabled={props.disabled} 
-                autoFocus />
-            {/* <button className="fa-solid fa-plus bg-transparent hover:bg-green-500 text-green-700 hover:text-white py-2 px-3 border border-green-500 rounded" onClick={onClickAddButton} disabled={props.disabled || !name}></button> */}
-        </div>
+                value={itemName}
+                onInput={onInput}
+                autoFocus
+                enterKeyHint='done'
+            />
+        </form>
     );
 }
