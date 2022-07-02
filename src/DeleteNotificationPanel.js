8b8b739566bfb7ccb992774755d7b511e4420e19
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-02 10:21:53 -0700

    change positions

--- src/DeleteNotificationPanel.js
+++ src/DeleteNotificationPanel.js
@@ -4,7 +4,7 @@ import DeleteNotification from './DeleteNotification';
 export default function DeleteNotificationPanel(props) {
 
     return (
-        <div className="fixed inset-0 flex items-end px-4 py-6 sm:p-6 pointer-events-none">
+        <div className="fixed inset-0 flex items-end px-4 py-6 sm:p-6 pointer-events-none sm:static">
             <div className="w-full flex flex-col items-center space-y-4">
                 {props.lastDeleted &&
                     <DeleteNotification
