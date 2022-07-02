22f07f8a6a0eb3aef4e740a57cdb34f1baf408f6
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-02 15:28:03 -0700

    make icon

--- src/DeleteButton.js
+++ src/DeleteButton.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { TrashIcon } from '@heroicons/react/outline'
 
 export default function DeleteButton(props) {
-    return <button className="bg-none border-none p-0 underline cursor-pointer text-gray-600 hover:text-black" onClick={props.onClick} disabled={props.disabled}>Delete</button>
+    return (
+        <div title="Delete item">
+            <TrashIcon
+                className="h-5 w-5 bg-none border-none underline cursor-pointer text-gray-600 hover:text-red-600"
+                onClick={props.onClick}
+                disabled={props.disabled}
+            />
+        </div>
+    );
 }
