badf081228b3c80be991afa619602cb49385dbdc
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-06 12:37:35 -0700

    update logo

--- src/Logo.js
+++ src/Logo.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Logo(props) {
     return (
-        <h1 className="font-medium leading-light text-5xl mt-5 select-none text-center">
-            listo
-            <span className="fa-stack text-[.35em] align-middle">
+        <h1 className="font-medium select-none text-2xl md:text-2xl flex-shrink-0 flex items-center">
+            <span className="text-gray-600">listo</span>
+            <span className="fa-stack text-[.35em]">
                 <i className="fa-solid fa-square fa-stack-2x text-green-700"></i>
                 <i className="fa-solid fa-check fa-stack-1x fa-inverse"></i>
             </span>
