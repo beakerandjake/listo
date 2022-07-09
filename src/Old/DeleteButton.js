a75be52dcec0b77e3812ef3e4aa3abfec203511c
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-09 11:29:30 -0700

    use font awesome

--- src/Old/DeleteButton.js
+++ src/Old/DeleteButton.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { TrashIcon } from '@heroicons/react/outline'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 export default function DeleteButton(props) {
     return (
         <div title="Delete item">
-            <TrashIcon
-                className="h-5 w-5 bg-none border-none underline cursor-pointer text-gray-600 hover:text-red-600"
-                onClick={props.onClick}
-                disabled={props.disabled}
+            <FontAwesomeIcon
+                icon={faTrashCan}
+                className="bg-none border-none underline cursor-pointer text-gray-600 hover:text-red-600"
+                {...props}
             />
         </div>
     );
