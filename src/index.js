7eba55659a1a7bab7a4c0df647441e3a7372ddfc
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-09 10:52:23 -0700

    use error boundary

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import ErrorBoundary from './components/ErrorBoundary';
+import { ErrorBoundary } from 'react-error-boundary';
 import './index.css';
+import App from './App';
+import Error from './components/Error';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ErrorBoundary>
+    <ErrorBoundary FallbackComponent={Error}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
