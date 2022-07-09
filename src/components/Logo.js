c401f1744ff4ceb6e967bfcf85d2a5dbe7b85c0a
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-09 10:38:37 -0700

    fix name

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ export default function Logo(props) {
     return (
         <span className="select-none flex-shrink-0 font-medium text-4xl md:text-4xl text-gray-600 whitespace-nowrap">
             listo
-            <i class="fa-solid fa-square-check text-3xl text-green-700 pl-1"></i>
+            <i className="fa-solid fa-square-check text-3xl text-green-700 pl-1"></i>
         </span>
     );
 }
