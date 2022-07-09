675a5adcdc244d2dc994aa8f923e35530d0923d1
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-09 12:51:26 -0700

    wip sidebar styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,20 +4,20 @@ import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function Sidebar(props) {
     const lists = props.items.map(item => (
-        <a key={item.id} href="/" lassName="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md">
-            <FontAwesomeIcon icon={faCirclePlus} className="text-gray-400 group-hover:text-gray-500 mr-3 flex-shrink-0 h-6 w-6"/>
+        <a key={item.id} href="/" className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md">
+            <FontAwesomeIcon icon={faCirclePlus} className="text-gray-400 group-hover:text-gray-500 mr-3 flex-shrink-0"/>
             {item.name}
         </a>
     ));
 
     return (
         <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
-            <div className="border-r border-gray-200 pt-5 flex flex-col flex-grow bg-white overflow-y-auto">
-                <div className="flex-shrink-0 px-4 flex items-center">
+            <div className="border-r border-gray-200 pt-5 flex flex-col flex-grow bg-white">
+                <div className="flex-shrink-0 px-4 flex items-center justify-center">
                     <Logo />
                 </div>
-                <div className="flex-grow mt-5 flex flex-col">
-                    <nav className="flex-1 px-2 pb-4 space-y-1">
+                <div className="flex-grow mt-5 flex flex-col ">
+                    <nav className="flex-1 px-2 pb-4 space-y-1 ">
                         {lists}
                     </nav>
                 </div>
@@ -31,3 +31,4 @@ export default function Sidebar(props) {
         </div >
     )
 }
+
