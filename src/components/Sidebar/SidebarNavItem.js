a5b6327b920f931dc01c2583ec08938258239b25
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-10 13:14:57 -0700

    wip routing

--- src/components/Sidebar/SidebarNavItem.js
+++ src/components/Sidebar/SidebarNavItem.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { getIcon } from 'services/listIconService';
 
@@ -6,9 +7,13 @@ export default function SidebarNavItem(props) {
     const selectedStyle = props.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900'
 
     return (
-        <a href="/" className={selectedStyle + " group flex items-center px-2 py-2 text-base font-medium rounded-md"}>
+        <Link to={`/lists/${props.id}`} className={selectedStyle + " group flex items-center px-2 py-2 text-base font-medium rounded-md"}>
             <FontAwesomeIcon icon={icon} fixedWidth className="text-gray-400 group-hover:text-gray-500 mr-3 flex-shrink-0" />
             {props.name}
-        </a>
+        </Link>
+        // <a href="/" className={selectedStyle + " group flex items-center px-2 py-2 text-base font-medium rounded-md"}>
+        //     <FontAwesomeIcon icon={icon} fixedWidth className="text-gray-400 group-hover:text-gray-500 mr-3 flex-shrink-0" />
+        //     {props.name}
+        // </a>
     );
 }
