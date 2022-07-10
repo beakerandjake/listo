55b56f210404a2ecafc282f278c4d8f9b535381a
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-10 13:28:35 -0700

    make logo a link

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 
 export default function Logo(props) {
     return (
-        <span className="select-none flex-shrink-0 font-medium text-4xl md:text-4xl text-gray-600 whitespace-nowrap">
+        <Link to="/" className="select-none flex-shrink-0 font-medium text-4xl md:text-4xl text-gray-600 whitespace-nowrap cursor-pointer">
             listo
             <FontAwesomeIcon icon={faSquareCheck} className="text-3xl text-green-700 pl-1" />
-        </span>
+        </Link>
     );
 }
