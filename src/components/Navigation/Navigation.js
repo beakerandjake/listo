bfcacf69534639e3a9afa3d16266c932c64d9124
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-12 13:14:07 -0700

    wip navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,68 +4,18 @@ import { Navbar } from './Navbar';
 import { Transition, Dialog } from '@headlessui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes, faTimesCircle, faTimesRectangle } from '@fortawesome/free-solid-svg-icons';
+import { CollapsibleSidebar } from './CollapsibleSidebar';
 
 export function Navigation(props) {
     const [menuOpen, setMenuOpen] = useState(false);
     console.log(menuOpen);
     return (
         <>
-            <Transition.Root show={menuOpen} as={Fragment}>
-                <Dialog as="div" className="relative z-40 md:hidden" onClose={setMenuOpen}>
-                    {/* Background Overlay */}
-                    <Transition.Child
-                        as={Fragment}
-                        enter="transition-opacity ease-linear duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="transition-opacity ease-linear duration-300"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black bg-opacity-60" />
-                    </Transition.Child>
-
-                    <div className="fixed inset-0 flex z-40">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="transition ease-in-out duration-300 transform"
-                            enterFrom="-translate-x-full"
-                            enterTo="translate-x-0"
-                            leave="transition ease-in-out duration-300 transform"
-                            leaveFrom="translate-x-0"
-                            leaveTo="-translate-x-full"
-                        >
-                            <Dialog.Panel className="relative flex-1 flex flex-col max-w-xs w-full bg-white">
-                                <Transition.Child
-                                    as={Fragment}
-                                    enter="ease-in-out duration-300"
-                                    enterFrom="opacity-0"
-                                    enterTo="opacity-100"
-                                    leave="ease-in-out duration-300"
-                                    leaveFrom="opacity-100"
-                                    leaveTo="opacity-0"
-                                >
-                                    <div className="absolute top-0 right-0 -mr-12 pt-2">
-                                        <button
-                                            type="button"
-                                            className="ml-1 flex items-center justify-center h-10 w-10"
-                                            onClick={() => setMenuOpen(false)}
-                                        >
-                                            <span className="sr-only">Close sidebar</span>
-                                            <FontAwesomeIcon icon={faTimes} size='2x' fixedWidth className="text-white" />
-                                        </button>
-                                    </div>
-                                </Transition.Child>
-
-                                <Sidebar items={props.items} />
-                            </Dialog.Panel>
-                        </Transition.Child>
-                        <div className="flex-shrink-0 w-14">{/* Force sidebar to shrink to fit close icon */}</div>
-                    </div>
-                </Dialog>
-            </Transition.Root>
-
-            <Navbar onClickOpenMenuButton={() => setMenuOpen(!menuOpen)} />
+            <CollapsibleSidebar open={menuOpen} onSetClose={() => setMenuOpen(false)}>
+                <div>Hello Sidebar!</div>
+            </CollapsibleSidebar>
+            <Navbar onClickOpenMenuButton={() => setMenuOpen(true)} />
+            
             <p>Menu Open: {menuOpen ? 'true' : 'false'}</p>
 
 
---
+++ src/components/Navigation/StaticSidebar.js
@@ -0,0 +1,7 @@
+
+
+export function StaticSidebar(props) {
+    return (
+        <div>Static Sidebar</div>
+    )
+}
