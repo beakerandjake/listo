1df7ad541313ed6f12190049595291776231a8b3
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-13 12:00:51 -0700

    move error into routes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 import './index.css';
 import App from './App';
-import Error from './components/Error';
+import Error from './routes/Error';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
