8feb139d3e65d71db7299986a07021d9b9c883e1
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-14 12:12:02 -0700

    add skeleton

--- src/routes/List/List.js
+++ src/routes/List/List.js
@@ -1,15 +1,19 @@
 import { useParams } from 'react-router-dom';
 import { PageHeader } from "components/PageHeader";
+import { useState } from 'react';
+import { Skeleton } from './Skeleton';
 
 export function List(props) {
     const { id } = useParams();
+    const { loading, setLoading } = useState(true);
 
     return (
         <>
-            <PageHeader name={'List: ' + id} />
+            <Skeleton />
+            {/* <PageHeader name={'List: ' + id} />
             <div className="py-4">
                 <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-            </div>
+            </div> */}
         </>
     )
 }
---
+++ src/routes/List/Skeleton.js
@@ -0,0 +1,17 @@
+
+export function Skeleton() {
+    const items = [...Array(3)].map(_ => (
+        <div className="flex justify-between items-center px-4 gap-2">
+            <div className="bg-slate-200 rounded-full w-6 h-6"></div>
+            <div className="bg-slate-200 flex-grow h-6 rounded-lg"></div>
+        </div>
+    ));
+
+    return (
+        <div className="flex flex-col gap-4 animate-pulse">
+            <div className="bg-slate-200 w-1/3 h-10 rounded-lg"></div>
+            <div className="bg-slate-200 w-full h-8 rounded-lg"></div>
+            {items}
+        </div>
+    )
+}
