3c37282a5e6cce048f782a9a1e8e94099f00021a
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-19 10:59:45 -0700

    styling input

--- src/routes/List/AddItem.js
+++ src/routes/List/AddItem.js
@@ -6,23 +6,35 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 export function AddItem(props) {
     const [input, setInput] = useState('');
 
+    const inputValid = () => {
+        return input.length > 1;
+    }
+
     return (
         <form className="w-full">
-            <div className="relative w-full">
-                <input
-                    type="text"
-                    value={input}
-                    onInput={e => setInput(e.target.value)}
-                    className="shadow appearance-none border-gray-200 w-full text-gray-700 focus:outline-none focus:ring-0 focus:border-inherit"
-                    placeholder={props.placeholder || 'What do we need?'}
-                    autoComplete="off"
-                    autoFocus
-                    enterKeyHint="done"
-                />
-                <div className="text-green-700 absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer">
-                    <FontAwesomeIcon icon={faPlus} className={classNames({'opacity-25 cursor-default': input.length < 2})} />
+            <div className="mt-1 flex">
+                <div className="relative flex items-stretch flex-grow">
+                    <div className="text-gray-400 absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                        <FontAwesomeIcon icon={faPlus} />
+                    </div>
+                    <input
+                        type="text"
+                        value={input}
+                        onInput={e => setInput(e.target.value)}
+                        className="block w-full pl-8 sm:text-sm border-gray-300 shadow focus:outline-none focus:ring-0 focus:border-inherit"
+                        placeholder={props.placeholder || 'Add Items...'}
+                        autoComplete="off"
+                        autoFocus
+                        enterKeyHint="done"
+                    />
+                    <button
+                        type="button"
+                        className={classNames(inputValid() ? "hover:bg-gray-100 cursor-pointer" : "text-opacity-20 cursor-not-allowed", "shadow -ml-px relative flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-gray-50 focus:outline-none focus:ring-0")}
+                    >
+                        Add
+                    </button>
                 </div>
             </div>
-        </form>
+        </form >
     )
 }
