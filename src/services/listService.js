1e6762111096475545c78e60c387386644f669d8
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-19 09:49:46 -0700

    update mock api data

--- src/services/listService.js
+++ src/services/listService.js
@@ -1,27 +1,92 @@
+// const lists = [
+//     { name: 'Grocery', id: '1', iconName: 'cart-shopping', count: 42 },
+//     { name: 'Todo', id: '2', iconName: 'list-check', count: 0 },
+//     { name: 'Trips', id: '3', iconName: 'plane', count: 14 },
+//     { name: 'Purchases', id: '4', iconName: 'dollar-sign', count: 10 }
+// ];
+
 const lists = [
-    { name: 'Grocery', id: '1', iconName: 'cart-shopping', count: 42 },
-    { name: 'Todo', id: '2', iconName: 'list-check', count: 0 },
-    { name: 'Trips', id: '3', iconName: 'plane', count: 14 },
-    { name: 'Purchases', id: '4', iconName: 'dollar-sign', count: 10 }
+    {
+        id: 1,
+        name: 'Grocery',
+        iconName: 'cart-shopping',
+        items: [
+            { id: 1, name: 'Bananas', quantity: 1 },
+            { id: 2, name: 'Apples', quantity: 4 },
+            { id: 3, name: 'Oranges', quantity: 5 },
+            { id: 4, name: 'Pears', quantity: 2 },
+            { id: 5, name: 'Peaches', quantity: 3 },
+        ]
+    },
+    {
+        id: 2,
+        name: 'Todo',
+        iconName: 'list-check',
+        items: [
+            { id: 1, name: 'Bananas', quantity: 1 },
+            { id: 2, name: 'Apples', quantity: 4 },
+            { id: 3, name: 'Oranges', quantity: 5 },
+            { id: 4, name: 'Pears', quantity: 2 },
+            { id: 5, name: 'Peaches', quantity: 3 },
+        ]
+    },
+    {
+        id: 3,
+        name: 'Trips',
+        iconName: 'plane',
+        items: [
+            { id: 1, name: 'Bananas', quantity: 1 },
+            { id: 2, name: 'Apples', quantity: 4 },
+            { id: 3, name: 'Oranges', quantity: 5 },
+            { id: 4, name: 'Pears', quantity: 2 },
+            { id: 5, name: 'Peaches', quantity: 3 },
+        ]
+    },
+    {
+        id: 4,
+        name: 'Purchases',
+        iconName: 'dollar-sign',
+        items: [
+            { id: 1, name: 'Bananas', quantity: 1 },
+            { id: 2, name: 'Apples', quantity: 4 },
+            { id: 3, name: 'Oranges', quantity: 5 },
+            { id: 4, name: 'Pears', quantity: 2 },
+            { id: 5, name: 'Peaches', quantity: 3 },
+        ]
+    }
 ];
+
 
 export async function getLists() {
     await new Promise(resolve => setTimeout(resolve, 0));
-    return lists;
-}
-
-const list = {
-    name: 'Grocery',
-    items: [
-        { id: 1, name: 'Bananas', quantity: 1 },
-        { id: 2, name: 'Apples', quantity: 4 },
-        { id: 3, name: 'Oranges', quantity: 5 },
-        { id: 4, name: 'Pears', quantity: 2 },
-        { id: 5, name: 'Peaches', quantity: 3 },
-    ]
+    const toReturn = lists.map(x => ({
+        name: x.name,
+        id: x.id,
+        iconName: x.iconName,
+        count: x.items.length
+    }));
+    return toReturn;
 }
 
 export async function getList(id) {
     await new Promise(resolve => setTimeout(resolve, 250));
-    return list;
+
+    const parsedId = parseInt(id);
+
+    if (isNaN(parsedId)) {
+        throw new Error(`Could not parse id: ${id}`);
+    }
+
+    const list = lists.find(x => x.id === parsedId);
+
+    if (!list) {
+        throw new Error(`Could not find list with id: ${id}`);
+    }
+
+    const toReturn = {
+        name: list.name,
+        items: list.items
+    };
+
+    return toReturn;
 }
