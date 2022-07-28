4329eeecea6f19bfe4c8cc5f22a07c496d61db9e
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-28 09:42:26 -0700

    add title

--- src/routes/List/Item/DeleteButton.js
+++ src/routes/List/Item/DeleteButton.js
@@ -3,7 +3,7 @@ import { faTimes, faTrash, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 export function DeleteButton(props) {
     return (
-        <button onClick={props.onClick} disabled={props.disabled}>
+        <button onClick={props.onClick} disabled={props.disabled} title="Delete Item">
             <FontAwesomeIcon icon={faTrashAlt} className="text-slate-300 hover:text-slate-500" />
         </button>
     )
--- src/routes/List/Item/EditButton.js
+++ src/routes/List/Item/EditButton.js
@@ -3,7 +3,7 @@ import { faPencil } from '@fortawesome/free-solid-svg-icons';
 
 export function EditButton(props) {
     return (
-        <button onClick={props.onClick} disabled={props.disabled}>
+        <button onClick={props.onClick} disabled={props.disabled} title="Edit Item">
             <FontAwesomeIcon icon={faPencil} className="text-slate-300 hover:text-slate-500" />
         </button>
     )
