1ce7f1de7e92a6211fd24db22132e42068b334cb
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-30 10:19:55 -0700

    add drawer component

---
+++ src/components/Drawer.js
@@ -0,0 +1,45 @@
+import { Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+export function Drawer(props) {
+    return (
+        <Transition.Root show={props.open} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={props.onClose}>
+                {/* Overlay */}
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+                {/* Main content */}
+                <div className="fixed inset-0 overflow-hidden">
+                    <div className="absolute inset-0 overflow-hidden">
+                        <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="transform transition ease-in-out duration-300"
+                                enterFrom="translate-x-full"
+                                enterTo="translate-x-0"
+                                leave="transform transition ease-in-out duration-300"
+                                leaveFrom="translate-x-0"
+                                leaveTo="translate-x-full"
+                            >
+                                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                                    <div className="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
+                                        {props.children}
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition.Root>
+    )
+}
