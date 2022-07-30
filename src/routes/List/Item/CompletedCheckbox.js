b454dfdcc085e3bfdc7ea4d22aa8175bf6875f15
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-29 18:17:58 -0700

    stop click event

--- src/routes/List/Item/CompletedCheckbox.js
+++ src/routes/List/Item/CompletedCheckbox.js
@@ -1,12 +1,13 @@
 
 export function CompletedCheckbox(props) {
 
-    const toggleValue = () => {
+    const toggleValue = e => {
+        e.stopPropagation();
         props.onChange(!props.checked);
     }
 
     return (
-        <div className="flex items-center p-2 cursor-pointer" onClick={toggleValue}>
+        <div className="flex items-center p-2 cursor-pointer" onClick={e => toggleValue(e)}>
             <input
                 type="checkbox"
                 className="focus:ring-green-500 text-green-700 border-gray-300 rounded cursor-pointer"
