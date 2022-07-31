b8d4f0e3b939901055e8e3bba0bf1ffa2486f447
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-31 14:45:19 -0700

    back to normal

--- src/components/Layout/DesktopLayout.js
+++ src/components/Layout/DesktopLayout.js
@@ -1,26 +1,12 @@
 export function DesktopLayout(props) {
     return (
         <>
-            {/* <div className="flex flex-col fixed inset-y-0 w-64">
+            <div className="flex flex-col fixed inset-y-0 w-64">
                 {props.sidebar}
-            </div> */}
-            <div className="flex flex-col flex-1">
+            </div>
+            <div className="pl-64 flex flex-col flex-1">
                 {props.children}
             </div>
         </>
     )
 }
-
-
-// export function DesktopLayout(props) {
-//     return (
-//         <>
-//             <div className="flex flex-col fixed inset-y-0 w-64">
-//                 {props.sidebar}
-//             </div>
-//             <div className="pl-64 flex flex-col flex-1">
-//                 {props.children}
-//             </div>
-//         </>
-//     )
-// }
