3f45a7e5fbd715f108afbfb48c77b667654efca4
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-31 14:53:45 -0700

    pass through props

--- src/routes/List/Item/NameLabel.js
+++ src/routes/List/Item/NameLabel.js
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 
 export function NameLabel(props) {
     return (
-        <p className={classNames({ 'line-through': props.completed })}>
+        <p className={classNames({ 'line-through': props.completed }, props.className)}>
             {props.name}
         </p>
     )
