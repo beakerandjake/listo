0834322d462d001b30eff0a667ef5d41b2413265
beakerandjake <beakerandjake@users.noreply.github.com>
2022-07-31 17:17:52 -0700

    dont allow text input

--- src/routes/List/EditItem/DueDatePicker.js
+++ src/routes/List/EditItem/DueDatePicker.js
@@ -6,6 +6,7 @@ import "react-datepicker/dist/react-datepicker.css";
 
 export function DueDatePicker(props) {
     const [date, setDate] = useState(null);
+    const [datePickerOpen, setDatePickerOpen] = useState(false);
 
     // Ensure the date passed in is always turned into a date. 
     useEffect(() => {
@@ -22,12 +23,20 @@ export function DueDatePicker(props) {
     return (
 
         <div className="flex w-full">
-            <ReactDatePicker
-                selected={date}
-                onChange={props.onChange}
-                placeholderText="Add Due Date"
-                className="block w-full text-gray-900 border-gray-300 focus:outline-none focus:ring-0"
-            />
+            <div className="w-full" onClick={() => setDatePickerOpen(!datePickerOpen)}>
+                <ReactDatePicker
+                    selected={date}
+                    onChange={date => {
+                        setDatePickerOpen(false);
+                        props.onChange(date);
+                    }}
+                    placeholderText="Add Due Date"
+                    className="block w-full text-gray-900 border-gray-300 focus:outline-none focus:ring-0"
+                    readOnly={true}
+                    onClickOutside={() => setDatePickerOpen(false)}
+                    open={datePickerOpen}
+                />
+            </div>
             <button
                 type="button"
                 className="-ml-px px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-0 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
