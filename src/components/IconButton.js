425f845633cff72071859435100fea05d0caf2c1
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-01 10:42:28 -0700

    change default text

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export function IconButton(props) {
-    const className = props.className || 'text-slate-300 hover:text-slate-500';
+    const className = props.className || 'text-gray-500 hover:text-gray-700';
 
     return (
         <button onClick={props.onClick} disabled={props.disabled} title={props.title} className="focus:outline-none">
