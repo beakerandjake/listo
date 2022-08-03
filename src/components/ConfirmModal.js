a0cbee7aa2183ab798d696e3be262555eabf2b30
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-03 14:04:35 -0700

    remove focus

--- src/components/ConfirmModal.js
+++ src/components/ConfirmModal.js
@@ -1,16 +1,12 @@
-import { Fragment, useRef } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { Button } from "components/Button";
 
 export function ConfirmModal(props) {
-    const cancelButtonRef = useRef(null);
-
     return (
-        <Transition.Root show={props.open} as={Fragment}>
-            <Dialog as="div" className="relative z-50" onClose={props.onDismiss} initialFocus={cancelButtonRef}>
+        <Transition.Root show={props.open}>
+            <Dialog as="div" className="relative z-50" onClose={props.onDismiss}>
                 {/* Overlay */}
                 <Transition.Child
-                    as={Fragment}
                     enter="ease-out duration-300"
                     enterFrom="opacity-0"
                     enterTo="opacity-100"
@@ -25,7 +21,6 @@ export function ConfirmModal(props) {
                 <div className="fixed z-10 inset-0 overflow-y-auto">
                     <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
                         <Transition.Child
-                            as={Fragment}
                             enter="ease-out duration-300"
                             enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                             enterTo="opacity-100 translate-y-0 sm:scale-100"
@@ -43,8 +38,9 @@ export function ConfirmModal(props) {
                                     </Dialog.Description>
                                 </div>
                                 <div className="flex justify-end items-center gap-2 bg-gray-50 px-4 py-3">
+
                                     <Button text={props.cancelButtonText || "Cancel"} onClick={props.onDismiss} />
-                                    <Button text={props.confirmButtonText} variant={props.variant} onClick={props.onConfirm} ref={cancelButtonRef} />
+                                    <Button text={props.confirmButtonText} variant={props.variant} onClick={props.onConfirm} />
                                 </div>
                             </Dialog.Panel>
                         </Transition.Child>
