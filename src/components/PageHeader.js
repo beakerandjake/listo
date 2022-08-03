56a5802a2757fbe0506279030a71c84f911cec44
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-03 12:41:01 -0700

    change style

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,3 +1,7 @@
 export function PageHeader(props) {
-    return <h1 className="text-2xl md:text-3xl font-semibold md:font-medium text-gray-900">{props.name}</h1>
+    return (
+        <h1 className="text-2xl font-bold md:font-medium leading-7 text-gray-900 sm:text-3xl sm:truncate">
+            {props.name}
+        </h1>
+    )
 }
