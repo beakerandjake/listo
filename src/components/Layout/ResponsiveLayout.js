ffd69b20ec1ae6fb2c26e9cee39bfd1aed6edca9
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-04 11:37:13 -0700

    add overflow to fix dropdown

--- src/components/Layout/ResponsiveLayout.js
+++ src/components/Layout/ResponsiveLayout.js
@@ -11,7 +11,7 @@ export function ResponsiveLayout(props) {
         </main>
     )
     return (
-        <div className="flex flex-col h-full">
+        <div className="flex flex-col h-full overflow-auto">
             {/* Desktop / Larger screens */}
             <MediaQuery minWidth={MOBILE_BREAKPOINT}>
                 <DesktopLayout sidebar={props.sidebar}>
