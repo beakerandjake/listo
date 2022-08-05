1ca67cc9fcb8e0cef9ec572d120bf7d30345e830
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-05 12:54:23 -0700

    can't focus toolbar if input not valid

--- src/routes/List/AddItem/AddItem.js
+++ src/routes/List/AddItem/AddItem.js
@@ -9,12 +9,10 @@ export function AddItem(props) {
     const [input, setInput] = useState('');
 
     // is the current input a valid length to be submitted?
-    const inputValid = () => {
-        return input && input.length > 1;
-    };
+    const inputValid = input && input.length > 1;
 
     const onAddItem = () => {
-        if (!inputValid() || props.disabled) {
+        if (!inputValid || props.disabled) {
             return;
         }
 
@@ -22,42 +20,42 @@ export function AddItem(props) {
         setInput('');
     };
 
-    const orig = (
-        <form className="w-full" onSubmit={onAddItem}>
-            <div className="mt-1 flex h-[50px]">
-                <div className="relative flex items-stretch flex-grow">
-                    <div className="text-gray-400 absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                        <FontAwesomeIcon icon={faPlus} />
-                    </div>
-                    <input
-                        type="text"
-                        value={input}
-                        onInput={e => setInput(e.target.value)}
-                        className="block w-full pl-8 sm:text-sm border-gray-300 shadow focus:outline-none focus:ring-0 focus:border-inherit"
-                        placeholder={props.placeholder || 'Add Item'}
-                        autoComplete="off"
-                        autoFocus
-                        enterKeyHint="done"
-                    />
-                    <button
-                        type="button"
-                        className={classNames(inputValid() ? "hover:bg-gray-100 cursor-pointer" : "text-opacity-20 cursor-not-allowed", "shadow -ml-px relative flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-gray-50 focus:outline-none focus:ring-0")}
-                        disabled={props.disabled || !inputValid()}
-                        onClick={onAddItem}
-                    >
-                        Add
-                    </button>
-                </div>
-            </div>
-        </form >
-    );
+    // const orig = (
+    //     <form className="w-full" onSubmit={onAddItem}>
+    //         <div className="mt-1 flex h-[50px]">
+    //             <div className="relative flex items-stretch flex-grow">
+    //                 <div className="text-gray-400 absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+    //                     <FontAwesomeIcon icon={faPlus} />
+    //                 </div>
+    //                 <input
+    //                     type="text"
+    //                     value={input}
+    //                     onInput={e => setInput(e.target.value)}
+    //                     className="block w-full pl-8 sm:text-sm border-gray-300 shadow focus:outline-none focus:ring-0 focus:border-inherit"
+    //                     placeholder={props.placeholder || 'Add Item'}
+    //                     autoComplete="off"
+    //                     autoFocus
+    //                     enterKeyHint="done"
+    //                 />
+    //                 <button
+    //                     type="button"
+    //                     className={classNames(inputValid() ? "hover:bg-gray-100 cursor-pointer" : "text-opacity-20 cursor-not-allowed", "shadow -ml-px relative flex items-center px-4 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-gray-50 focus:outline-none focus:ring-0")}
+    //                     disabled={props.disabled || !inputValid()}
+    //                     onClick={onAddItem}
+    //                 >
+    //                     Add
+    //                 </button>
+    //             </div>
+    //         </div>
+    //     </form >
+    // );
 
     return (
         <div className="bg-blue-50 shadow divide-y divide-gray-200">
             <AddItemInput value={input} onChange={setInput} onSubmit={onAddItem} />
             <AddItemToolbar
                 onClickAddButton={onAddItem}
-                disabled={props.disabled || !inputValid()}
+                inputValid={inputValid}
             />
         </div>
     )
--- src/routes/List/AddItem/AddItemToolbar.js
+++ src/routes/List/AddItem/AddItemToolbar.js
@@ -3,12 +3,16 @@ import { Button } from 'components/Button';
 
 export function AddItemToolbar(props) {
     return (
-        <Toolbar.Root className="p-2 bg-gray-200 rounded-b flex items-center justify-between">
+        <Toolbar.Root className="p-2 bg-gray-200 rounded-b flex items-center justify-between" tabIndex={props.inputValid ? 0 : -1}>
             <div>
                 ASDF
             </div>
             <Toolbar.Button asChild>
-                <Button text="Add" disabled={props.disabled} onClick={props.onClickAddButton} />
+                <Button
+                    text="Add"
+                    disabled={!props.inputValid}
+                    onClick={props.onClickAddButton}
+                />
             </Toolbar.Button>
         </Toolbar.Root>
 
