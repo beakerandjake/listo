ae41b130259f8e0eb80f56af09bfdbbd2e9685c7
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-04 17:00:22 -0700

    use drawer

--- src/components/Navigation/CollapsibleSidebarContainer.js
+++ src/components/Navigation/CollapsibleSidebarContainer.js
@@ -1,66 +1,14 @@
-import { Fragment } from 'react';
-import { Transition, Dialog } from '@headlessui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Drawer, DrawerClose } from 'components/Drawer';
 
 export function CollapsibleSidebarContainer(props) {
     return (
-        <Transition.Root show={props.open} as={Fragment}>
-            <Dialog as="div" className="relative z-40" onClose={props.onSetClose}>
-                {/* Background Overlay */}
-                <Transition.Child
-                    as={Fragment}
-                    enter="transition-opacity ease-linear duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="transition-opacity ease-linear duration-300"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="fixed inset-0 bg-black bg-opacity-60" />
-                </Transition.Child>
-
-                <div className="fixed inset-0 flex z-40">
-                    <Transition.Child
-                        as={Fragment}
-                        enter="transition ease-in-out duration-300 transform"
-                        enterFrom="-translate-x-full"
-                        enterTo="translate-x-0"
-                        leave="transition ease-in-out duration-300 transform"
-                        leaveFrom="translate-x-0"
-                        leaveTo="-translate-x-full"
-                    >
-                        <Dialog.Panel className="relative flex-1 flex flex-col max-w-xs w-full bg-white">
-                            {/* Close Button */}
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-in-out duration-300"
-                                enterFrom="opacity-0"
-                                enterTo="opacity-100"
-                                leave="ease-in-out duration-300"
-                                leaveFrom="opacity-100"
-                                leaveTo="opacity-0"
-                            >
-                                <div className="absolute top-0 right-0 -mr-12 pt-2">
-                                    <button
-                                        type="button"
-                                        className="ml-1 flex items-center justify-center h-10 w-10 outline-none rounded-full "
-                                        onClick={props.onSetClose}
-                                    >
-                                        <FontAwesomeIcon icon={faTimes} size="xl" className="text-white font-thin" />
-                                    </button>
-                                </div>
-                            </Transition.Child>
-
-                            {/* Sidebar Main Content */}
-                            <div className="flex-1 h-0 overflow-y-auto">
-                                {props.children}
-                            </div>
-                        </Dialog.Panel>
-                    </Transition.Child>
-                    <div className="flex-shrink-0 w-14">{/* Force sidebar to shrink to fit close icon */}</div>
-                </div>
-            </Dialog>
-        </Transition.Root>
+        <Drawer open={props.open} onClose={props.onSetClose} size="xs" side="left">
+            {props.children}
+            <DrawerClose className="absolute top-0 right-0 -mr-9 mt-2 pt-2" title="Close Navigation">
+                <FontAwesomeIcon icon={faTimes} size="xl" className="text-white font-thin" />
+            </DrawerClose >
+        </Drawer>
     )
 }
