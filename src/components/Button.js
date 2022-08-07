6130d79eec0e96d1ee6f6cf6f803b17c160796ac
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-07 14:51:46 -0700

    button sizes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,22 @@
 import { forwardRef } from 'react';
 import classNames from 'classnames';
 
-const variants = {
+const VARIANTS = {
     danger: 'text-white bg-red-600 enabled:hover:bg-red-700 focus:ring-red-500',
     success: 'bg-green-700 text-green-100 enabled:hover:bg-green-800',
     default: 'text-gray-700 bg-white enabled:hover:bg-gray-50 focus:ring-indigo-500'
-}
+};
+
+const SIZES = {
+    sm: 'px-2 py-1 text-xs',
+    default: 'px-3 py-2 text-sm'
+};
 
 export const Button = forwardRef((props, ref) => {
-    const { className, variant, ...leftOverProps } = props;
+    const { className, variant, size, ...leftOverProps } = props;
 
-    const variantStyle = variants[variant] || variants.default;
+    const variantStyle = VARIANTS[variant] || VARIANTS.default;
+    const sizeStyle = SIZES[size] || SIZES.default;
 
     return (
         <button
@@ -18,7 +24,8 @@ export const Button = forwardRef((props, ref) => {
             ref={ref}
             className={classNames(
                 variantStyle,
-                'inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md',
+                sizeStyle,
+                'inline-flex items-center border border-gray-300 shadow-sm font-medium rounded-md',
                 'disabled:cursor-not-allowed disabled:opacity-50 focus:outline-none focus:ring-2 focus:ring-offset-2',
                 className
             )}
