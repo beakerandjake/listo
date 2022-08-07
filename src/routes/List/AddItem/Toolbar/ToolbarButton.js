905d364c0f9007b18fc6ef61de689a84c6f54f95
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-07 12:16:14 -0700

    make radix toolbar button

--- src/routes/List/AddItem/Toolbar/ToolbarButton.js
+++ src/routes/List/AddItem/Toolbar/ToolbarButton.js
@@ -7,19 +7,20 @@ export const ToolbarButton = forwardRef((props, ref) => {
     const { icon, text, className, ...rest } = props;
 
     return (
-        <RadixToolbar.Button
-            {...rest}
-            ref={ref}
-            className={cx(
-                'p-1 flex items-center justify-between gap-1 leading-0',
-                'cursor-pointer disabled:cursor-not-allowed disabled:opacity-50',
-                'focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-indigo-500',
-                'rounded shadow-sm border border-gray-300 bg-white enabled:hover:bg-gray-50 text-gray-700',
-                className
-            )}
-        >
-            <FontAwesomeIcon icon={icon} fixedWidth />
-            {text && <span className="text-sm">{text}</span>}
+        <RadixToolbar.Button asChild>
+            <button
+                {...rest}
+                ref={ref}
+                className={cx(
+                    'p-1 flex items-center justify-between gap-1 leading-0',
+                    'cursor-pointer disabled:cursor-not-allowed disabled:opacity-50',
+                    'focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-indigo-500',
+                    'rounded shadow-sm border border-gray-300 bg-white enabled:hover:bg-gray-50 text-gray-700',
+                    className
+                )}>
+                <FontAwesomeIcon icon={icon} fixedWidth />
+                {text && <span className="text-sm">{text}</span>}
+            </button>
         </RadixToolbar.Button>
     )
 });
