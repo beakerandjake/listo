6fe6781b823be43b730557df6859c20b12b1ca3d
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-08 14:58:07 -0700

    no select

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,6 +1,6 @@
 export function PageHeader(props) {
     return (
-        <h1 className="text-2xl font-bold md:font-medium leading-7 text-gray-900 sm:text-3xl sm:truncate">
+        <h1 className="text-2xl font-bold md:font-medium leading-7 text-gray-900 sm:text-3xl sm:truncate select-none">
             {props.name}
         </h1>
     )
