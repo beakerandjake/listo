2f5216d42df161f1361def7a5cf044e38e84b8c5
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-08 18:35:34 -0700

    ring only when focus-visible

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,9 @@ import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const VARIANTS = {
-    danger: 'text-white bg-red-600 enabled:hover:bg-red-700 focus:ring-red-500',
+    danger: 'text-white bg-red-600 enabled:hover:bg-red-700 focus-visible:ring-red-500',
     success: 'bg-green-700 text-green-100 enabled:hover:bg-green-800',
-    default: 'text-gray-700 bg-white enabled:hover:bg-gray-50 focus:ring-indigo-500'
+    default: 'text-gray-700 bg-white enabled:hover:bg-gray-50 focus-visible:ring-indigo-500'
 };
 
 const SIZES = {
@@ -27,7 +27,7 @@ export const Button = forwardRef((props, ref) => {
                 variantStyle,
                 sizeStyle,
                 'flex items-center justify-between gap-2 border border-gray-300 shadow-sm font-medium rounded-md',
-                'disabled:cursor-not-allowed disabled:opacity-50 focus:outline-none focus:ring-2 focus:ring-offset-2',
+                'disabled:cursor-not-allowed disabled:opacity-50 keyboard-only-ring',
                 className
             )}
         >
