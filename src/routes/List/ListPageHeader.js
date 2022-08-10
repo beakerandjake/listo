7db8d8b69c3dbb2109783918dfd0e9a7d803670e
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-10 14:57:01 -0700

    re-add border bottom

--- src/routes/List/ListPageHeader.js
+++ src/routes/List/ListPageHeader.js
@@ -5,7 +5,7 @@ import { getIcon } from "services/iconLibrary";
 
 export function ListPageHeader(props) {
     return (
-        <div className="flex flex-wrap items-center gap-3">
+        <div className="flex flex-wrap items-center gap-3 border-b border-gray-200 pb-3 mb-3 sm:border-b-0 sm:pb-0 sm:mb-0">
             <FontAwesomeIcon icon={getIcon(props.iconName)} size="xl" className="text-gray-500 hidden sm:block" />
             <PageHeader name={props.name} />
             {props.children}
