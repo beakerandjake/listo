e9910d66196c89da916bbd8a7b7a861842a89711
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-10 15:48:36 -0700

    temp quantity button

--- src/routes/List/AddItem/Toolbar/AddItemToolbar.js
+++ src/routes/List/AddItem/Toolbar/AddItemToolbar.js
@@ -1,5 +1,6 @@
 import * as Toolbar from '@radix-ui/react-toolbar';
 import { Button } from 'components/Button';
+import { QuantityButton } from 'routes/List/Item/QuantityButton';
 import { DueDateButton } from './DueDateButton';
 
 export const AddItemToolbarButton = Toolbar.Button;
@@ -7,7 +8,10 @@ export const AddItemToolbarButton = Toolbar.Button;
 export function AddItemToolbar(props) {
     return (
         <Toolbar.Root className="px-3 py-2 bg-gray-100 rounded-b flex items-center justify-between">
-            <DueDateButton dueDate={props.item.dueDate} onDueDateChange={dueDate => props.onItemChange({ dueDate })} />
+            <div className="flex items-center gap-3">
+                <DueDateButton dueDate={props.item.dueDate} onDueDateChange={dueDate => props.onItemChange({ dueDate })} />
+                <QuantityButton quantity={props.item.quantity} onQuantityChange={quantity => props.onItemChange({ quantity })} />
+            </div>
 
             <Toolbar.Button asChild>
                 <Button
