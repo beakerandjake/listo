2dab2f054f270c973299af188e60634dcf184cb4
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-11 11:36:27 -0700

    only define sizes/anchors once

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -7,74 +7,111 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export const DrawerTitle = Dialog.Title;
 export const DrawerClose = Dialog.Close;
 
-const ORIENTATIONS = {
-    v: 'vertical',
-    h: 'horizontal'
-};
-
 const SIZES = {
     xs: 'xs',
     sm: 'sm',
     md: 'md',
     lg: 'lg',
+    xl: 'xl',
     full: 'full'
-}
+};
 
-const SIDES = {
+const ANCHORS = {
     left: 'left',
     right: 'right',
     bottom: 'bottom'
-}
+};
 
-const SIZE_STYLES2 = [
+const SIZE_STYLES = [
+    // HORIZONTAL ANCHORS
     {
         size: SIZES.xs,
-        
-        value: ''
+        anchors: [ANCHORS.left, ANCHORS.right],
+        className: 'w-full max-w-xs'
     },
     {
-        key: SIZES.sm,
-        value: ''
-    }, {
-        key: SIZES.md,
-        value: ''
-    }, {
-        key: SIZES.lg,
-        value: ''
-    }, {
-        key: SIZES.xl,
-        value: ''
+        size: SIZES.sm,
+        anchors: [ANCHORS.left, ANCHORS.right],
+        className: 'w-full max-w-sm'
+    },
+    {
+        size: SIZES.md,
+        anchors: [ANCHORS.left, ANCHORS.right],
+        className: 'w-full max-w-md'
+    },
+    {
+        size: SIZES.lg,
+        anchors: [ANCHORS.left, ANCHORS.right],
+        className: 'w-full max-w-lg'
+    },
+    {
+        size: SIZES.xl,
+        anchors: [ANCHORS.left, ANCHORS.right],
+        className: 'w-full max-w-xl'
+    },
+    {
+        size: SIZES.full,
+        anchors: [ANCHORS.left, ANCHORS.right],
+        className: 'w-full'
+    },
+    // HORIZONTAL ANCHORS
+    {
+        size: SIZES.xs,
+        anchors: [ANCHORS.bottom],
+        className: 'h-1/6'
+    },
+    {
+        size: SIZES.sm,
+        anchors: [ANCHORS.bottom],
+        className: 'h-2/6'
+    },
+    {
+        size: SIZES.md,
+        anchors: [ANCHORS.bottom],
+        className: 'h-3/6'
+    },
+    {
+        size: SIZES.lg,
+        anchors: [ANCHORS.bottom],
+        className: 'h-4/6'
+    },
+    {
+        size: SIZES.xl,
+        anchors: [ANCHORS.bottom],
+        className: 'h-5/6'
+    },
+    {
+        size: SIZES.full,
+        anchors: [ANCHORS.bottom],
+        className: 'h-full'
+    }
+];
+
+const ANCHOR_STYLES = [
+    {
+        anchor: ANCHORS.left,
+        className: 'inset-y-0 left-0'
+    },
+    {
+        anchor: ANCHORS.right,
+        className: 'inset-y-0 right-0'
+    },
+    {
+        anchor: ANCHORS.bottom,
+        className: 'inset-x-0 bottom-0'
     },
 ]
 
-const SIZE_STYLES = {
-    vertical: {
-        xs: 'w-screen max-w-xs',
-        sm: 'w-screen max-w-sm',
-        md: 'w-screen max-w-md',
-        lg: 'w-screen max-w-lg',
-        full: 'w-screen'
+const CLOSE_BUTTON_ANCHOR_STYLES = [
+    {
+        anchor: ANCHORS.right,
+        className: 'top-3 right-3'
     },
-    horizontal: {
-        xs: 'h-1/4',
-        sm: 'h-1/3',
-        md: 'h-1/2',
-        lg: 'h-2/3',
-        xl: 'h-3/4',
-        full: 'h-full',
+    {
+        anchor: ANCHORS.left,
+        className: 'top-3 left-3'
     }
-}
-
-const SIDE_STYLES = {
-    left: 'inset-y-0 left-0',
-    right: 'inset-y-0 right-0',
-    bottom: 'inset-x-0 bottom-0'
-}
-
-const CLOSE_BUTTON_SIDE_STYLES = {
-    right: 'top-3 right-3',
-    left: 'top-3 left-3'
-};
+];
 
 const CLOSE_BUTTON_DEFAULT_ICON = faTimes;
 
@@ -82,24 +119,15 @@ function invalidProp(message) {
     throw new Error(message);
 }
 
-function getOrientation(side) {
-    return ['left', 'right'].includes(side) ? ORIENTATIONS.v : ORIENTATIONS.h;
-}
-
-/**
- * Sample.
- * @param {Object} props - The Props.
- * @param {'left'|'right'} props.side - The side of the drawer that the close button will appear on.
- */
-function DefaultCloseButton({ side, icon, title }) {
-    const sideStyle = CLOSE_BUTTON_SIDE_STYLES[side] || invalidProp(`Unsupported closeButtonSide ${side}`);
+function DefaultCloseButton({ anchor, icon, title }) {
+    const anchorStyle = CLOSE_BUTTON_ANCHOR_STYLES.find(x => x.anchor === anchor)?.className || invalidProp(`Unsupported anchor for close button: '${anchor}'`);
 
     return (
         <DrawerClose
             className={cx(
                 'absolute w-5 h-5 rounded-full flex items-center justify-center',
                 'text-gray-400 hover:text-gray-500 keyboard-only-focus-ring',
-                sideStyle
+                anchorStyle
             )}
             title={title}
         >
@@ -113,34 +141,34 @@ function DefaultCloseButton({ side, icon, title }) {
  * @param {Object} props - The Props.
  * @param {boolean} props.open - Should the drawer currently be opened or closed?
  * @param {function} props.onClose - Callback fired when the user requests to close the drawer
- * @param {'right'|'left'|'bottom'=} props.side - The side of the viewport that the Drawer is anchored to. 
+ * @param {'right'|'left'|'bottom'=} props.anchor - The side of the viewport that the Drawer is anchored to. 
  * @param {'xs'|'sm'|'md'|'lg'|'xl'|'full'=} props.size - How much of the viewport should the drawer take up? 
  * @param {boolean=} props.showCloseButton - Should a default show button be rendered?
- * @param {'left'|'right'=} props.closeButtonSide - The side of the drawer that the default show button will appear on.
+ * @param {'left'|'right'=} props.closeButtonAnchor - The side of the drawer that the default show button will be anchored to.
  * @param {IconDefinition=}  props.closeButtonIcon - The FontAwesomeIcon of the default show button.
  * @param {string}  props.closeButtonTitle - The text to display on close button hover.
  */
 export function Drawer({
     open = false,
     onClose,
-    side = 'right',
-    size = 'md',
-    showCloseButton = true,
-    closeButtonSide = 'right',
+    anchor = ANCHORS.right,
+    size = SIZES.md,
+    showCloseButton = false,
+    closeButtonAnchor = ANCHORS.right,
     closeButtonIcon = CLOSE_BUTTON_DEFAULT_ICON,
     closeButtonTitle = 'Close',
     children
 }, z) {
     const drawerRef = useRef(null);
-    const sideStyle = SIDE_STYLES[side] || invalidProp(`Unsupported side: ${side}`);
-    const sizeStyle = SIZE_STYLES[getOrientation(side)][size] || invalidProp(`Unsupported size: ${size}`);
+    const anchorStyle = ANCHOR_STYLES.find(x => x.anchor === anchor)?.className || invalidProp(`Unsupported anchor: '${anchor}'`);
+    const sizeStyle = SIZE_STYLES.find(x => x.size === size && x.anchors.includes(anchor))?.className || invalidProp(`Unsupported size: '${size}'`);
 
     return (
         <Dialog.Root open={open} onOpenChange={open => !open && onClose()}>
             <Dialog.Portal className="z-10">
                 <Dialog.Overlay className="z-10 fixed inset-0 bg-black bg-opacity-50 transition-opacity" tabIndex={-1} />
                 <Dialog.Content
-                    className={cx(sizeStyle, sideStyle, "z-10 fixed shadow-xl bg-white focus:outline-none")}
+                    className={cx(sizeStyle, anchorStyle, " z-10 fixed shadow-xl bg-white focus:outline-none")}
                     onOpenAutoFocus={e => {
                         e.preventDefault();
                         drawerRef.current.focus();
@@ -148,7 +176,7 @@ export function Drawer({
                     ref={drawerRef}
                 >
                     {children}
-                    {!!showCloseButton && DefaultCloseButton({ side: closeButtonSide, icon: closeButtonIcon, title: closeButtonTitle })}
+                    {!!showCloseButton && DefaultCloseButton({ anchor: closeButtonAnchor, icon: closeButtonIcon, title: closeButtonTitle })}
                 </Dialog.Content>
             </Dialog.Portal>
         </Dialog.Root >
--- src/components/Navigation/CollapsibleSidebarContainer.js
+++ src/components/Navigation/CollapsibleSidebarContainer.js
@@ -4,7 +4,7 @@ import { Drawer, DrawerClose } from 'components/Drawer';
 
 export function CollapsibleSidebarContainer(props) {
     return (
-        <Drawer open={props.open} onClose={props.onSetClose} size="xs" side="left">
+        <Drawer open={props.open} onClose={props.onSetClose} size="xs" anchor="left">
             {props.children}
             <DrawerClose className="absolute top-0 right-0 -mr-9 mt-2 pt-2" title="Close Navigation">
                 <FontAwesomeIcon icon={faTimes} size="xl" className="text-white font-thin" />
