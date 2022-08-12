2c940286f8325b22270d85d70825f838a20bd6fc
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-12 16:15:03 -0700

    wip testing responsive menu

--- src/components/Menu/ResponsiveMenu.js
+++ src/components/Menu/ResponsiveMenu.js
@@ -1,8 +1,63 @@
+import { Drawer } from 'components/Drawer';
+import MediaQuery from 'react-responsive';
+import { MOBILE_BREAKPOINT } from 'services/responsiveUtilities';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuPortal,
+    DropdownMenuTrigger
+} from './TempDropdown';
 
-export function ResponsiveMenu(props){
+// open = false,
+// onClose,
+// anchor = ANCHORS.right,
+// size = SIZES.md,
+// showCloseButton = false,
+// closeButtonAnchor = ANCHORS.right,
+// closeButtonIcon = CLOSE_BUTTON_DEFAULT_ICON,
+// closeButtonTitle = 'Close',
+// children
+
+export function ResponsiveMenu({
+    open,
+    onClose,
+    children,
+    mobileAnchor = 'bottom',
+    mobileSize = 'xl',
+    mobileShowCloseButton = true,
+    mobileCloseButtonIcon = null,
+    mobileCloseButtonTitle = null
+}) {
+
     return (
         <div>
-            Hello World
-        </div>
+            {/* On larger screens, render a floating dropdown menu */}
+            <MediaQuery minWidth={MOBILE_BREAKPOINT}>
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        ASDF
+                    </DropdownMenuTrigger>
+                    <DropdownMenuPortal>
+                        <DropdownMenuContent>
+                            {children}
+                        </DropdownMenuContent>
+                    </DropdownMenuPortal>
+                </DropdownMenu>
+            </MediaQuery>
+            {/* On smaller screens, render a drawer menu */}
+            <MediaQuery maxWidth={MOBILE_BREAKPOINT}>
+                <Drawer
+                    open={open}
+                    onClose={onClose}
+                    anchor={mobileAnchor}
+                    size={mobileSize}
+                    showCloseButton={mobileShowCloseButton}
+                    mobileCloseButtonIcon={mobileCloseButtonIcon}
+                    closeButtonTitle={mobileCloseButtonTitle}
+                >
+                    {children}
+                </Drawer>
+            </MediaQuery>
+        </div >
     )
 }
---
+++ src/components/Menu/TempDropdown.js
@@ -0,0 +1,6 @@
+import * as RadixDropdownMenu from '@radix-ui/react-dropdown-menu';
+
+export const DropdownMenu = RadixDropdownMenu.Root;
+export const DropdownMenuTrigger = RadixDropdownMenu.Trigger;
+export const DropdownMenuPortal = RadixDropdownMenu.Portal;
+export const DropdownMenuContent = RadixDropdownMenu.Content;
