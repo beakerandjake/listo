d6be96f7b7fe619e054c3e05d888ceb21b8c9e03
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-12 12:52:34 -0700

    wip calendar component

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,21 @@
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { format } from 'date-fns';
+import ReactCalendar from 'react-calendar';
+
+// import 'react-calendar/dist/Calendar.css';
+import './Calendar.css';
+
+export function Calendar({ value, onChange }) {
+    return <ReactCalendar
+        calendarType='US'
+        value={value}
+        onChange={onChange}
+        formatShortWeekday={(locale, date) => format(date, 'EEEEEE')}
+        prevLabel={<FontAwesomeIcon icon={faChevronLeft} />}
+        prev2Label={null}
+        nextLabel={<FontAwesomeIcon icon={faChevronRight} />}
+        next2Label={null}
+        minDetail="year"
+    />
+}
