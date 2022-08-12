89c0302b3d24c0b0b1ba7e2c3738b47f7b0019a7
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-12 15:30:26 -0700

    merge classname

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -1,4 +1,5 @@
 import { forwardRef } from "react";
+import cx from 'classnames';
 import { Button } from "./Button";
 
 // Styled button which just displays an icon with small amount of padding.
@@ -9,7 +10,10 @@ export const IconButton = forwardRef((props, ref) => {
         <Button
             {...rest}
             ref={ref}
-            className="bg-inherit shadow-none text-gray-500 enabled:hover:text-gray-700 enabled:hover:bg-inherit text-base p-1"
+            className={cx(
+                'bg-inherit shadow-none text-gray-500 enabled:hover:text-gray-700 enabled:hover:bg-inherit text-base p-1',
+                className
+            )}
             border="none"
             size="custom"
         />
