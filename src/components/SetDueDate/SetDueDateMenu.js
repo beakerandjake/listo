8abf5f6e7fc580a13b97a7cf2a6d93cec5b1d123
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-14 12:28:22 -0700

    making menu responsive

--- src/components/SetDueDate/SetDueDateMenu.js
+++ src/components/SetDueDate/SetDueDateMenu.js
@@ -1,15 +1,20 @@
-import { faArrowLeft, faArrowRightFromBracket, faArrowsLeftRightToLine, faBackwardStep, faCalendarCheck, faCalendarDay, faCalendarDays, faCalendarWeek, faChevronLeft, faChevronRight, faLeftLong, faTimes, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Calendar } from "components/Calendar";
-import { Drawer } from "components/Drawer";
-import { IconButton } from "components/IconButton";
-import { MenuHeader, MenuItem, MenuItemLabel, MenuSeparator, MenuTitle, ScrollableMenuContent } from "components/Menu";
+import { useEffect, useState } from "react";
 import { format, nextMonday, startOfToday, startOfTomorrow } from "date-fns";
-import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faCalendarCheck, faCalendarDay, faCalendarDays, faCalendarWeek, faChevronRight, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { Calendar } from "components/Calendar";
+import {
+    MenuHeader,
+    MenuItem,
+    MenuItemLabel,
+    MenuSeparator,
+    MenuTitle,
+    ResponsiveMenu,
+    ScrollableMenuContent
+} from "components/Menu";
 
 
 function StaticDueDateMenuItem({ icon, date, text, onClick }) {
-
     return (
         <MenuItem icon={icon} label={text} onClick={onClick}>
             <MenuItemLabel className="text-gray-400 font-semibold">
@@ -19,8 +24,16 @@ function StaticDueDateMenuItem({ icon, date, text, onClick }) {
     )
 };
 
-export function SetDueDateMenu({ dueDate, onDueDateChange, onOpenCustomDueDate }) {
-    const [subMenuOpen, setSubMenuOpen] = useState(false);
+export function SetDueDateMenu({ open, onClose, trigger, dueDate, onDueDateChange, }) {
+    const [subMenuOpen, setSubMenuOpen] = useState(true);
+
+    // Any time the menu is closed reset the sub menu state.
+    useEffect(() => {
+        if (!open) {
+            console.log('close sub menu!')
+            setSubMenuOpen(false);
+        }
+    }, [open])
 
     const staticDates = [
         {
@@ -36,46 +49,48 @@ export function SetDueDateMenu({ dueDate, onDueDateChange, onOpenCustomDueDate }
             icon: faCalendarWeek,
             date: nextMonday(new Date())
         }
-    ].map(x => {
-        return <StaticDueDateMenuItem key={x.text} {...x} onClick={() => onDueDateChange(x.date)} />
-    });
+    ].map(x => (<StaticDueDateMenuItem key={x.text} {...x} onClick={() => onDueDateChange(x.date)} />));
+
+    const subMenuTrigger = (
+        <MenuItem icon={faCalendarDays} label="Custom Due Date" onClick={() => setSubMenuOpen(true)}>
+            <FontAwesomeIcon icon={faChevronRight} className="text-gray-500 group-hover:text-gray-700" />
+        </MenuItem>
+    );
 
     return (
-        <>
+        <ResponsiveMenu open={open} onClose={onClose} trigger={trigger}>
             <MenuHeader className="flex items-center justify-center">
                 <MenuTitle>Add Due Date</MenuTitle>
             </MenuHeader>
             <ScrollableMenuContent>
+                {/* Static Date Buttons */}
                 {staticDates}
                 <MenuSeparator />
-                <MenuItem icon={faCalendarDays} label="Custom Due Date" onClick={() => setSubMenuOpen(true)}>
-                    <FontAwesomeIcon icon={faChevronRight} className="text-gray-500 group-hover:text-gray-700" />
-                </MenuItem>
+                {/* Custom Due Date Sub Menu */}
+                <ResponsiveMenu
+                    open={subMenuOpen}
+                    onClose={() => setSubMenuOpen(false)}
+                    trigger={subMenuTrigger}
+                    desktopSide="right"
+                    desktopAlign="start"
+                    mobileCloseButtonAnchor="left"
+                    mobileCloseButtonIcon={faArrowLeft}
+                >
+                    <MenuHeader className="flex items-center justify-center">
+                        <MenuTitle className="">Custom Due Date</MenuTitle>
+                    </MenuHeader>
+                    <ScrollableMenuContent className="mb-2">
+                        <Calendar value={dueDate} onChange={onDueDateChange} />
+                    </ScrollableMenuContent>
+                </ResponsiveMenu>
+                {/* Remove Due Date Button */}
                 {!!dueDate && (
                     <>
                         <MenuSeparator />
                         <MenuItem icon={faTrashAlt} label="Remove Due Date" variant="danger" onClick={() => onDueDateChange(null)} />
                     </>
                 )}
-
             </ScrollableMenuContent>
-            {/* Set Custom Date Sub Menu! */}
-            <Drawer
-                open={subMenuOpen}
-                onClose={() => setSubMenuOpen(false)}
-                anchor="bottom"
-                size="xl"
-                showCloseButton={true}
-                closeButtonAnchor="left"
-                closeButtonIcon={faArrowLeft}
-            >
-                <MenuHeader className="flex items-center justify-center">
-                    <MenuTitle className="">Custom Due Date</MenuTitle>
-                </MenuHeader>
-                <ScrollableMenuContent className="mb-2">
-                    <Calendar value={dueDate} onChange={onDueDateChange} />
-                </ScrollableMenuContent>
-            </Drawer>
-        </>
+        </ResponsiveMenu>
     )
 }
