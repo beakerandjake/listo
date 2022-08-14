524de9b866746fba83e815f1c738a89713305d25
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-14 11:57:27 -0700

    pass style

--- src/components/Menu/TempDropdown.js
+++ src/components/Menu/TempDropdown.js
@@ -7,25 +7,31 @@ export const DropdownMenuPortal = RadixDropdownMenu.Portal;
 export const DropdownMenuContent = RadixDropdownMenu.Content;
 
 
-export function DropdownMenuContentStyled(props) {
-    const { children, ...rest } = props;
+export function DropdownMenuContentStyled({
+    side = 'bottom',
+    align = 'end',
+    sideOffset = 5,
+    collisionPadding = 10,
+    children,
+    ...props
+}) {
     const z = useRef(null);
     return (
         <RadixDropdownMenu.Portal>
             <RadixDropdownMenu.Content
-                side={"bottom"}
-                align={"end"}
-                sideOffset={5}
-                collisionPadding={10}
+                side={side}
+                align={align}
+                sideOffset={sideOffset}
+                collisionPadding={collisionPadding}
                 className="flex flex-col min-w-[14rem] rounded-md shadow-lg bg-white ring-1 ring-offset-1 ring-gray-300 focus:outline-none z-[3] overflow-hidden"
-                {...rest}
+                {...props}
                 onOpenAutoFocus={e => {
                     e.preventDefault();
                     z.current.focus();
                 }}
                 ref={z}
             >
-                {props.children}
+                {children}
             </RadixDropdownMenu.Content>
         </RadixDropdownMenu.Portal>
     );
