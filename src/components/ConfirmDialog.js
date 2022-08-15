162caa8fbc53fc9daca4d48da8b7f7adb498788a
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-15 12:22:05 -0700

    update confirm dialog

--- src/components/ConfirmDialog.js
+++ src/components/ConfirmDialog.js
@@ -1,5 +1,6 @@
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Transition } from '@headlessui/react';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import cx from 'classnames';
 import { Button } from "components/Button";
 import { IconButton } from './IconButton';
 import {
@@ -10,32 +11,46 @@ import {
     DialogTitle
 } from 'components/Dialog';
 
+
+/**
+ * Dialog which handles common scenario of confirming or cancelling an action.
+ * @param {Object} props - The Props.
+ * @param {boolean} props.open - Should the Dialog currently be opened or closed?
+ * @param {function} props.onDismiss - Callback fired when the user cancels the action.
+ * @param {function} props.onconfirm - Callback fired when the user confirms the action.
+ * @param {string} props.title - The title of the dialog.
+ * @param {string} props.message - The message of the dialog.
+ * @param {string=} props.cancelButtonText - Override text of the cancel button .
+ * @param {string} props.confirmButtonText - Text of the confirm button.
+ * @param {'success'|'danger'=} props.variant - Optional variant style to apply
+ */
 export function ConfirmDialog({
     open,
     onDismiss,
     onConfirm,
     title,
     message,
-    cancelButtonText,
+    cancelButtonText = 'Cancel',
     confirmButtonText,
-    variant,
-    children
+    variant
 }) {
-
-    // <Transition.Child
-    // enter="transform transition ease-out duration-300"
-    // enterFrom="-translate-full"
-    // enterTo="translate-y-0"
-    // leave="transform transition ease-in duration-200"
-    // leaveFrom="translate-y-0"
-    // leaveTo="translate-y-full"
-    // >
-
     return (
         <Dialog open={open} onClose={onDismiss} className="z-20">
             <DialogBackdrop />
-            <div className="fixed inset-0 overflow-y-auto">
-                <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+            <div
+                className={cx(
+                    'fixed inset-0 p-4 sm:p-0',
+                    'flex items-end sm:items-center justify-center min-h-full text-center'
+                )}
+            >
+                <Transition.Child
+                    enter="transform transition opacity ease-out duration-300"
+                    enterFrom="translate-y-full opacity-50"
+                    enterTo="translate-y-0 opacity-100"
+                    leave="transform transition opacity ease-in duration-200"
+                    leaveFrom="translate-y-0 opacity-100"
+                    leaveTo="translate-y-full opacity-75"
+                >
                     <DialogContent className="bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg w-full">
                         {/* Main Content */}
                         <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4 ">
@@ -68,7 +83,7 @@ export function ConfirmDialog({
                             <IconButton icon={faTimes} onClick={onDismiss} />
                         </div>
                     </DialogContent>
-                </div>
+                </Transition.Child>
             </div>
         </Dialog>
     )
