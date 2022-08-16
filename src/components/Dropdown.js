c08f842808e8bd3634de905a0363ca19bd1729c7
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-16 14:18:40 -0700

    wip simplifying dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,25 +1,16 @@
-import { Children, useLayoutEffect, isValidElement, cloneElement, useRef, useState, forwardRef, useMemo, useEffect } from 'react';
+import { Children, isValidElement, cloneElement, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Transition } from '@headlessui/react';
 import {
     useFloating,
-    useInteractions,
-    useDismiss,
     autoUpdate,
     offset,
     flip,
     shift,
-    useListNavigation,
-    useFloatingParentNodeId,
-    FloatingTree,
-    useFloatingTree,
-    useFloatingNodeId,
-    FloatingNode,
-    FloatingPortal,
-    FloatingFocusManager
-} from '@floating-ui/react-dom-interactions';
-import { mergeRefs } from "react-merge-refs";
-
+} from '@floating-ui/react-dom';
+import FocusLock from 'react-focus-lock';
+import { useOnClickOutside } from 'hooks/useOnClickOutside';
+import { useKeyDown } from 'hooks/useKeyDown';
 
 function iterateChildren(children, listItemsRef, getItemProps) {
     return Children.map(children, (child, index) => {
@@ -47,118 +38,57 @@ function iterateChildren(children, listItemsRef, getItemProps) {
     })
 }
 
-
-const DropdownComponent = forwardRef(({
+export const Dropdown = ({
     open,
+    onClickOutside,
+    onEscapeKeyDown,
     onClose,
     trigger,
     children,
     placement = 'bottom-end'
-}, ref) => {
-
-    const listItemsRef = useRef([]);
-    const [activeIndex, setActiveIndex] = useState(null);
-    const tree = useFloatingTree();
-    const nodeId = useFloatingNodeId();
-    const parentId = useFloatingParentNodeId();
-    const nested = !!parentId;
-
-    const { x, y, reference, floating, strategy, context, } = useFloating({
-        open,
-        onOpenChange: value => {
-            console.log('Dropdown - onOpenChange: ', value);
-            !value && onClose()
-        },
+}) => {
+    const { x, y, reference, floating, strategy, refs, } = useFloating({
         middleware: [offset(5), flip(), shift()],
         whileElementsMounted: autoUpdate,
         placement: placement,
-        strategy: 'fixed',
-        nodeId
+        strategy: 'fixed'
     });
 
-    // Hook into dom interactions.
-    const { getReferenceProps, getFloatingProps, getItemProps } = useInteractions([
-        useDismiss(context),
-        useListNavigation(context, {
-            listRef: listItemsRef,
-            activeIndex: activeIndex,
-            onNavigate: setActiveIndex,
-            loop: true,
-        })
-    ]);
+    const listItemsRef = useRef([]);
+    const [activeIndex, setActiveIndex] = useState(null);
 
-    // Support external trigger. 
-    // useLayoutEffect(() => {
-    //     reference(triggerElement.current);
-    // }, [triggerElement]);
-
-    const menuItems = iterateChildren(children, listItemsRef, getItemProps);
-
-    const mergedReferenceRef = useMemo(() => mergeRefs([ref, reference]), [
-        reference,
-        ref
-    ]);
-
-    const newTrigger = cloneElement(trigger, getReferenceProps({
-        ref: mergedReferenceRef,
-    }))
+    useOnClickOutside(refs.floating, e => onClickOutside && onClickOutside(e), !open);
+    useKeyDown('Escape', () => onEscapeKeyDown && onEscapeKeyDown(), !open);
 
     return (
-        <FloatingNode id={nodeId}>
-            {newTrigger}
-            <FloatingPortal>
-                <Transition
-                    show={open}
-                    enter="transition duration-100 ease-out"
-                    enterFrom="opacity-0"
-                    enterTo=" opacity-100"
-                    leave="transition duration-75 ease-out"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <FloatingFocusManager
-                        context={context}
-                        preventTabbing
-                        order={["reference", "content"]}
+        <>
+            {/* Dropdown Trigger */}
+            {cloneElement(trigger, { ref: reference })}
+
+            {/* Dropdown Content */}
+            <Transition
+                show={open}
+                enter="transition duration-100 ease-out"
+                enterFrom="opacity-0"
+                enterTo=" opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <FocusLock autoFocus={false}>
+                    <div
+                        ref={floating}
+                        className="absolute top-0 left-0 min-w-[14rem] rounded-md shadow-lg flex flex-col overflow-hidden bg-white ring-1 ring-offset-1 ring-gray-300 focus:outline-none"
+                        style={{
+                            position: strategy,
+                            top: y ?? 0,
+                            left: x ?? 0,
+                        }}
                     >
-                        <div
-                            {...getFloatingProps({
-                                ref: floating,
-                                className: 'absolute top-0 left-0 min-w-[14rem] rounded-md shadow-lg flex flex-col overflow-hidden bg-white ring-1 ring-offset-1 ring-gray-300 focus:outline-none',
-                                style: {
-                                    position: strategy,
-                                    top: y ?? 0,
-                                    left: x ?? 0,
-                                }
-                            })}
-                        >
-                            {menuItems}
-                        </div>
-                    </FloatingFocusManager>
-                </Transition>
-            </FloatingPortal>
-        </FloatingNode>
+                        {children}
+                    </div>
+                </FocusLock>
+            </Transition>
+        </>
     )
-});
-
-export const Dropdown = forwardRef((props, ref) => {
-    const parentId = useFloatingParentNodeId();
-
-    if (parentId === null) {
-        return (
-            <FloatingTree>
-                <DropdownComponent {...props} ref={ref} />
-            </FloatingTree>
-        );
-    }
-
-    return <DropdownComponent {...props} ref={ref} />;
-});
-
-Dropdown.propTypes = {
-    __type: PropTypes.string
-};
-
-Dropdown.defaultProps = {
-    __type: 'Dropdown'
 };
