c2a12adbb7a63ab3cbbd5739164792f8bf6caf18
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-18 07:55:04 -0700

    add hook

---
+++ src/hooks/usePreviousValue.js
@@ -0,0 +1,13 @@
+import { useEffect, useRef } from "react";
+
+/**
+ * Returns the value from the last render.
+ * @param {any} value - The value to cache the previous version of.
+ */
+export const usePreviousValue = (value) => {
+    const ref = useRef();
+    useEffect(() => {
+        ref.current = value;
+    })
+    return ref.current;
+}
