bbf4eed6e11a44e2531a96504ee17b9d774bf6d9
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-18 08:28:49 -0700

    wip updating toolbar

--- src/routes/List/AddItem/Toolbar/AddItemToolbar.js
+++ src/routes/List/AddItem/Toolbar/AddItemToolbar.js
@@ -1,28 +1,31 @@
-import * as Toolbar from '@radix-ui/react-toolbar';
 import { Button } from 'components/Button';
 import { QuantityButton } from 'routes/List/Item/QuantityButton';
 import { DueDateButton } from './DueDateButton';
 
-export const AddItemToolbarButton = Toolbar.Button;
-
-export function AddItemToolbar(props) {
+/**
+ * Toolbar which allows the user to edit properties of the item.
+ * @param {Object} props - The props.
+ * @param {object} props.item - The item being added.
+ * @param {function} props.onItemChange - Callback invoked when the user changes a property of the item. 
+ * @param {boolean} props.canAddItem - Is the item in a valid state to be added to the list? 
+ * @param {function} props.onAddItem - Callback invoked when the user clicks the add Item button. 
+ */
+export function AddItemToolbar({
+    item,
+    onItemChange,
+    canAddItem,
+    onAddItem
+}) {
     return (
-        <Toolbar.Root className="px-3 py-2 bg-gray-100 rounded-b flex items-center justify-between">
+        <div className="px-3 py-2 bg-gray-100 rounded-b flex items-center justify-between">
+            {/* Item edit buttons */}
             <div className="flex items-center gap-3">
-                <DueDateButton dueDate={props.item.dueDate} onDueDateChange={dueDate => props.onItemChange({ dueDate })} />
+                <DueDateButton dueDate={item.dueDate} onDueDateChange={dueDate => onItemChange({ dueDate })} />
                 {/* <QuantityButton quantity={props.item.quantity} onQuantityChange={quantity => props.onItemChange({ quantity })} /> */}
             </div>
-
-            <Toolbar.Button asChild>
-                <Button
-                    size="sm"
-                    disabled={!props.canAddItem}
-                    onClick={props.onAddItem}
-                >
-                    Add
-                </Button>
-            </Toolbar.Button>
-        </Toolbar.Root>
+            {/* Add item button */}
+            <Button size="sm" disabled={!canAddItem} onClick={onAddItem}>Add</Button>
+        </div>
 
     )
 }
--- src/routes/List/AddItem/Toolbar/ToolbarButton.js
+++ src/routes/List/AddItem/Toolbar/ToolbarButton.js
@@ -1,26 +1,32 @@
 import { forwardRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import cx from 'classnames';
-import * as RadixToolbar from '@radix-ui/react-toolbar';
 
-export const ToolbarButton = forwardRef((props, ref) => {
-    const { icon, text, className, ...rest } = props;
-
+/**
+ * Button styled for the Add Item Toolbar.
+ * @param {Object} props - The props.
+ * @param {IconDefinition} props.icon - The icon to display.
+ * @param {string=} props.text - The text to display (if any). 
+ * @param {string=} props.className - Additional styles to apply. 
+ */
+export const ToolbarButton = forwardRef(({
+    icon,
+    text,
+    className,
+    ...props
+}, ref) => {
     return (
-        <RadixToolbar.Button asChild>
-            <button
-                {...rest}
-                ref={ref}
-                className={cx(
-                    'p-1 flex items-center justify-between gap-1 leading-0',
-                    'cursor-pointer disabled:cursor-not-allowed disabled:opacity-50',
-                    'focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-indigo-500',
-                    'rounded shadow-sm border border-gray-300 bg-white enabled:hover:bg-gray-50 text-gray-700',
-                    className
-                )}>
-                <FontAwesomeIcon icon={icon} fixedWidth />
-                {text && <span className="text-sm">{text}</span>}
-            </button>
-        </RadixToolbar.Button>
+        <button
+            {...props}
+            ref={ref}
+            className={cx(
+                'p-1 flex items-center justify-between gap-1 leading-0 keyboard-only-focus-ring',
+                'cursor-pointer disabled:cursor-not-allowed disabled:opacity-50',
+                'rounded shadow-sm border border-gray-300 bg-white enabled:hover:bg-gray-50 text-gray-700',
+                className
+            )}>
+            <FontAwesomeIcon icon={icon} fixedWidth />
+            {text && <span className="text-sm">{text}</span>}
+        </button>
     )
 });
