3dc364c804a76ec01a31a0d186b21c9342f1c5e4
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-19 14:36:20 -0700

    add option to stop event

--- src/hooks/useKeyDown.js
+++ src/hooks/useKeyDown.js
@@ -6,8 +6,9 @@ import { useEffect } from "react";
  * @param {string} key - The key to detect.
  * @param {function} callback - The function that is invoked when the key is pressed.
  * @param {boolean} disabled - Should this effect be disabled?
+ * @param {boolean} stopEvent - Should the events propagation be cancelled?
  */
-export function useKeyDown(key, callback, disabled) {
+export function useKeyDown(key, callback, disabled, stopEvent) {
     useEffect(() => {
         if (!!disabled) {
             return;
@@ -18,7 +19,14 @@ export function useKeyDown(key, callback, disabled) {
                 return;
             }
 
+            if (stopEvent) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+
             callback();
+
+
         };
 
         document.addEventListener('keydown', onKeyDown);
