f8de92a4145af02a90d3101d52abb580a96d336c
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-20 12:41:30 -0700

    comment

--- src/routes/List/ListPageHeader.js
+++ src/routes/List/ListPageHeader.js
@@ -1,14 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PageHeader } from "components/PageHeader";
+import React from "react";
 import { getIcon } from "services/iconLibrary";
 
-
-export function ListPageHeader(props) {
+/**
+ * Header for the List page. 
+ * @param {Object} props
+ * @param {string} props.iconName - Name of the icon to render for the list.
+ * @param {function} props.name - Name of the list.
+ * @param {React.ReactNode} props.children - Additional items to render in the header.
+ */
+export function ListPageHeader({
+    iconName,
+    name,
+    children
+}) {
     return (
         <div className="flex flex-wrap items-center gap-3 border-b border-gray-200 pb-2 mb-2 sm:border-b-0 sm:pb-0 sm:mb-0">
-            <FontAwesomeIcon icon={getIcon(props.iconName)} size="xl" className="text-gray-500 hidden sm:block" />
-            <PageHeader name={props.name} />
-            {props.children}
+            <FontAwesomeIcon
+                icon={getIcon(iconName)}
+                size="xl"
+                className="text-gray-500 hidden sm:block"
+            />
+            <PageHeader name={name} />
+            {children}
         </div>
     )
 }
