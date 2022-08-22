6ac8654dc21070a83128e9df3198185a6093ec5c
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-22 12:29:08 -0700

    clean transition, fix dynamic class name tailwind issue

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -11,44 +11,10 @@ import {
 } from "react-transition-group";
 
 /**
-* Appends a '!' prefix to each className in the string.. 
-* @param {string} classNames - A string of space separated classNames.
-* @returns {array} An array of all classNames, with the ! prefixed to each one.
-**/
-const addImportantPrefix = (classNames) =>
-    classNames
-        .split(' ')
-        .filter((className) => className.trim().length > 1)
-        // Don't prefix classes transition-x utilities.
-        // That way they can be over-ridden by other utility classes like duration-x
-        .map(x => x.startsWith('transition') ? x : `!${x}`)
-        .join(' ') || '';
-
-/**
-* Creates a classNames object which can be passed to a CSSTransition component.
-* But most importantly appends a ! modifier to each *Active class. 
-* This ensures that enterActive/exitActive classes take priority over enter/exit classes. 
-* Without this there would be conflicts between those classes,
-* and since their css specificity would be the same there would be inconsistent results when animating.  
-* @param {Object} props
-* @param {string} props.icon - The icon to display.
-* @param {string} props.text - The text to display.
-* @param {string=} props.className - Additional styles to apply to the component
-*/
-const generateClassNames = (
-    enter = '',
-    enterActive = '',
-    exit = '',
-    exitActive = '',
-) => ({
-    enter: enter,
-    enterActive: addImportantPrefix(enterActive),
-    exit: exit,
-    exitActive: addImportantPrefix(exitActive),
-})
-
-/**
  * Wrapper around react-transition-group's CSSTransition component that allows easier usage with TailwindCSS. 
+ * Due to how CSSTransition and Tailwind work there is likely to be specificity conflicts during enter/enterActive, exit/exitActive.
+ * A workaround is to prefix the *Active classNames with a '!', that way they take precedence over the original style.
+ * For example if enter has 'opacity-0' and enterActive has 'opacity-100', change enterActive to be '!opacity-100'.
  * @param {Object} props
  * @param {string} props.enter - Classes to apply during enter phase.
  * @param {string} props.enterActive - Classes to apply during the entire enter phase.
@@ -58,10 +24,10 @@ const generateClassNames = (
  * @param {React.ReactNode} props.children - The child elements to render.
  **/
 export const Transition = ({
-    enter,
-    enterActive,
-    exit,
-    exitActive,
+    enter = '',
+    enterActive = '',
+    exit = '',
+    exitActive = '',
     classNames,
     children,
     ...props
@@ -76,7 +42,7 @@ export const Transition = ({
         <CSSTransition
             {...props}
             nodeRef={nodeRef}
-            classNames={classNames || generateClassNames(enter, enterActive, exit, exitActive)}
+            classNames={classNames || { enter, enterActive, exit, exitActive }}
             addEndListener={(done) => nodeRef.current.addEventListener("transitionend", done, false)}
         >
             {cloneElement(childrenSafe, { ref: (ref) => (nodeRef.current = ref) })}
@@ -118,7 +84,7 @@ export const SwitchTransition = ({
                     {...props}
                     key={switchKey}
                     nodeRef={nodeRef}
-                    classNames={classNames || generateClassNames(enter, enterActive, exit, exitActive)}
+                    classNames={classNames || { enter, enterActive, exit, exitActive }}
                     addEndListener={(done) => nodeRef.current.addEventListener("transitionend", done, false)}
                 >
                     {cloneElement(childrenSafe, { ref: (ref) => (nodeRef.current = ref) })}
