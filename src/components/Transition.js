d8d8811d8d28dfccad636f4e7118113b9c8a6bed
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-23 12:02:09 -0700

    add pre-defined transitions

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -2,6 +2,7 @@ import {
     cloneElement,
     createRef,
     isValidElement,
+    useEffect,
     useMemo,
     useRef
 } from "react";
@@ -82,3 +83,65 @@ export const SwitchTransition = ({
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [switchKey, children]);
 };
+
+/**
+ * Wrapper around react-transition-group's Transition which caches children when exiting.
+ * @param {Object} props
+ * @param {React.ElementType} props.as - Customize which type of Transition gets rendered.
+ * @param {React.ReactNode} props.children - The child elements to render.
+ **/
+export const MemoizedTransition = ({
+    as,
+    children,
+    ...props
+}) => {
+    const ComponentToRender = as || Transition;
+    const previousChildren = useRef(null);
+
+    useEffect(() => {
+        previousChildren.current = children;
+    }, [props.in, children]);
+
+    return (
+        <ComponentToRender
+            {...props}
+            unmountOnExit
+        >
+            {props.in ? children : previousChildren.current}
+        </ComponentToRender>
+    );
+};
+
+/**
+ * Transition which Fades in and out.
+ **/
+export const Fade = (props) => {
+    return (
+        <Transition
+            {...props}
+            classNames={{
+                enter: 'opacity-0',
+                enterActive: 'transition-opacity duration-300 !opacity-100',
+                exit: 'opacity-100',
+                exitActive: 'transition-opacity !opacity-0',
+            }}
+        />
+    );
+}
+
+/**
+ * Transition which Fades/Scales in and out.
+ **/
+export const FadeAndPop = (props) => {
+    return (
+        <Transition
+            {...props}
+            classNames={{
+                enter: 'opacity-0 scale-75',
+                enterActive: 'transition-[opacity,transform] duration-300 !opacity-100 !scale-100',
+                exit: 'opacity-100 scale-100',
+                exitActive: 'transition-[opacity,transform] !opacity-0 !scale-75',
+            }}
+        />
+    );
+}
