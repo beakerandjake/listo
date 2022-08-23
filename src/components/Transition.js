471ba89e96d83dbe2c53d8c5f349afad3c9cee77
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-23 15:36:04 -0700

    easing on fade

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -124,11 +124,11 @@ export const Fade = (props) => {
             {...props}
             classNames={{
                 appear: 'opacity-0',
-                appearActive: 'transition-opacity duration-300 !opacity-100',
+                appearActive: 'transition-opacity ease-in duration-300 !opacity-100',
                 enter: 'opacity-0',
-                enterActive: 'transition-opacity duration-300 !opacity-100',
+                enterActive: 'transition-opacity ease-in duration-300 !opacity-100',
                 exit: 'opacity-100',
-                exitActive: 'transition-opacity !opacity-0',
+                exitActive: 'transition-opacity ease-out !opacity-0',
             }}
         />
     );
