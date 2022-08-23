7fcbe985301a6afb9b33f1eddda99a67b340a5b9
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-23 16:52:38 -0700

    remove custom duration

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -124,9 +124,9 @@ export const Fade = (props) => {
             {...props}
             classNames={{
                 appear: 'opacity-0',
-                appearActive: 'transition-opacity ease-in duration-300 !opacity-100',
+                appearActive: 'transition-opacity ease-in !opacity-100',
                 enter: 'opacity-0',
-                enterActive: 'transition-opacity ease-in duration-300 !opacity-100',
+                enterActive: 'transition-opacity ease-in !opacity-100',
                 exit: 'opacity-100',
                 exitActive: 'transition-opacity ease-out !opacity-0',
             }}
