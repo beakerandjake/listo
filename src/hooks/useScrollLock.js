165fe6d98adc79426b0d3270fa9405b378da8279
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-27 13:31:24 -0700

    handle multiple locks

--- src/hooks/useScrollLock.js
+++ src/hooks/useScrollLock.js
@@ -1,21 +1,45 @@
-import { useCallback, useLayoutEffect, useRef } from "react";
+import { useCallback, useRef } from "react";
 
+let lockCount = 0;
 
-export const useScrollLock = (enabled) => {
+/**
+ * Locks scrolling on the body. Automatically handles multiple locks applied and only removes lock when the last lock is removed.
+ */
+export const useScrollLock = () => {
     const scrollPosition = useRef(0);
 
     const lockScroll = useCallback(() => {
+        lockCount += 1;
+
+        // if we've already applied the lock, don't apply it again.
+        if (lockCount > 1) {
+            return;
+        }
+
         document.body.style.overflow = 'hidden';
 
         // get the current scroll location of the body before fixing the position
         scrollPosition.current = window.scrollY;
+
         document.body.style.position = 'fixed';
         document.body.style.width = '100%';
+
         // maintain the current scroll position by offsetting the body by that amount. 
         document.body.style.top = `-${scrollPosition.current}px`;
     }, []);
 
     const unlockScroll = useCallback(() => {
+        // don't unlock if not locked.
+        if (lockCount <= 0) {
+            return;
+        }
+
+        lockCount -= 1;
+
+        // don't unlock if more locks are held in the lock queue.
+        if (lockCount > 0) {
+            return;
+        }
 
         // undo all custom styles we used to prevent scroll.
         document.body.style.overflow = '';
@@ -27,8 +51,14 @@ export const useScrollLock = (enabled) => {
         window.scrollTo(0, scrollPosition.current);
     }, []);
 
+    const forceUnlockScroll = useCallback(() => {
+        lockCount = 1;
+        unlockScroll();
+    }, [unlockScroll]);
+
     return {
         lockScroll,
-        unlockScroll
+        unlockScroll,
+        forceUnlockScroll
     }
 }
