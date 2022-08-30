1bd900f33d699b12fad6f14f883155e83f55cea2
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-30 09:16:45 -0700

    add clear input button

--- src/routes/List/Item/ItemNameInput.js
+++ src/routes/List/Item/ItemNameInput.js
@@ -1,18 +1,22 @@
-import { faPencil } from '@fortawesome/pro-solid-svg-icons';
+import { faPencil, faTimes } from '@fortawesome/pro-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import cx from 'classnames';
 import { useRef, useState } from 'react';
 import { EditItemMenuButton } from '../EditItem/EditItemMenuButton';
 import { itemValidationConstants } from '.';
+import { FadeAndPopIn } from 'components/Transition';
+import { IconButton } from 'components/IconButton';
 
 export const ItemNameInput = ({
     value,
     onChange
 }) => {
+
+    const inputIsBlank = value?.length <= 0;
+
     return (
         <div
             className={cx(
-
                 'relative transition-colors'
             )}
         >
@@ -21,7 +25,7 @@ export const ItemNameInput = ({
                     icon={faPencil}
                     fixedWidth
                     className={cx(
-                        value?.length > 0 ? 'text-indigo-700' : 'text-gray-400',
+                        inputIsBlank ? 'text-gray-400' : 'text-indigo-700',
                         'transition-colors'
                     )}
                 />
@@ -33,11 +37,21 @@ export const ItemNameInput = ({
                 placeholder="Item N&zwnj;ame"
                 className={cx(
                     'min-h-[3.5rem] w-full',
-                    'rounded border border-gray-300 placeholder-gray-400 [&:not(:focus)]:hover:bg-slate-100 pl-11'
+                    'rounded border border-gray-300 placeholder-gray-400 [&:not(:focus)]:hover:bg-slate-100 pl-11 pr-10'
                 )}
                 autoComplete="off"
                 maxLength={itemValidationConstants.maxNameLength}
             />
+            {/* Clear input Button */}
+            <FadeAndPopIn in={!inputIsBlank} unmountOnExit>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-3">
+                    <IconButton
+                        icon={faTimes}
+                        onClick={() => onChange('')}
+                        title="Clear Item Name"
+                    />
+                </div>
+            </FadeAndPopIn>
         </div>
     );
 };
