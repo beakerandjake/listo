98a499e95d8f059513f25578bd145fea9447db39
beakerandjake <beakerandjake@users.noreply.github.com>
2022-08-31 08:08:27 -0700

    first pass style

--- src/routes/List/ListTabGroup.js
+++ src/routes/List/ListTabGroup.js
@@ -11,23 +11,22 @@ const Tab = ({
         <div
             className={cx(
                 active
-                    ? 'bg-green-700 text-white'
+                    ? 'bg-green-700 text-white shadow-md'
                     : 'text-gray-500 hover:text-gray-700',
-                'px-3 py-2 font-medium text-sm rounded-md',
-                'cursor-pointer flex gap-2'
+                'px-3 py-2 flex gap-2 cursor-pointer rounded-md',
+                'font-medium text-md'
             )}
         >
             <span>{name}</span>
-            <Badge variant={active ? 'inverse' : 'default'}>{count}</Badge>
+            <Badge variant={active ? 'inverse' : 'default'} size="lg">{count}</Badge>
         </div>
     );
 }
 
 
 export const ListTabGroup = ({ children }) => {
-
     return (
-        <div className="flex items-center justify-between border-b-2 border-color-gray-600 pb-2 pr-2">
+        <div className="flex items-center justify-between">
             <div className="flex flex-1 space-x-4">
                 <Tab name="Active" active count={7} />
                 <Tab name="Complete" count={4} />
