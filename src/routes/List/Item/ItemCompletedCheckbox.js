ef0d3995f7e556df86f54c613bbe709517548ba6
beakerandjake <beakerandjake@users.noreply.github.com>
2022-09-19 12:37:47 -0700

    cleanup

--- src/routes/List/Item/ItemCompletedCheckbox.js
+++ src/routes/List/Item/ItemCompletedCheckbox.js
@@ -1,26 +1,35 @@
 import { useState } from "react";
 import cx from 'classnames';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareCheck as faSquareCheckSolid } from '@fortawesome/pro-solid-svg-icons';
 import { faSquare, faSquareCheck as faSquareCheckRegular } from "@fortawesome/pro-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export function ItemCompletedCheckbox(props) {
+/**
+ * Checkbox which lets a user mark an item as complete or incomplete.
+ * @param {Object} props
+ * @param {boolean} props.checked - The value of the checkbox.
+ * @param {function} props.onChange - Callback invoked when the user changes the value.
+ */
+export function ItemCompletedCheckbox({
+    checked,
+    onChange
+}) {
     const [hover, setHover] = useState(false);
 
+    // Toggle the checkbox on keypress.
     const onKeyDown = e => {
-        if (e.keyCode === 32 || e.key === "Enter") {
-            props.onChange(!props.checked);
+        if (e.key === " " || e.key === "Enter") {
+            onChange(!checked);
         }
     }
 
-    const toggleValue = e => {
+    // Toggle the checkbox on click.
+    const onClick = e => {
         e.stopPropagation();
-        props.onChange(!props.checked);
+        onChange(!checked);
     }
 
-    const style = props.checked ? 'text-green-700' : 'text-gray-400'
-
-    let icon = props.checked
+    let icon = checked
         ? faSquareCheckSolid
         : (hover ? faSquareCheckRegular : faSquare);
 
@@ -28,16 +37,17 @@ export function ItemCompletedCheckbox(props) {
         <div
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
-            className={"flex items-center justify-center w-8 h-8 rounded cursor-pointer group focus:outline-none"}
-            onClick={toggleValue}
+            className="flex items-center justify-center w-8 h-8 rounded cursor-pointer group focus:outline-none"
+            onClick={onClick}
             onKeyDown={onKeyDown}
             tabIndex={0}
         >
             <FontAwesomeIcon
                 icon={icon}
                 className={cx(
-                    style,
-                    "rounded group-focus-visible:outline-none group-focus-visible:ring-2 group-focus-visible:ring-offset-2 group-focus-visible:ring-green-700 group-focus"
+                    checked ? 'text-green-700' : 'text-gray-400',
+                    'rounded group-focus group-focus-visible:outline-none group-focus-visible:ring-2',
+                    'group-focus-visible:ring-offset-2 group-focus-visible:ring-green-700'
                 )}
             />
         </div>
