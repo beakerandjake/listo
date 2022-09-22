b89158c296e44d980bc5cb1a9dbc5f1db52f61fd
beakerandjake <beakerandjake@users.noreply.github.com>
2022-09-22 07:24:29 -0700

    scaffold post

--- src/routes/lists.js
+++ src/routes/lists.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middleware/validateRequest.js';
 import { getDb } from '../persistence/sqlite2.js';
 
 const router = express.Router();
@@ -16,5 +18,19 @@ router.get('/', (req, res) => {
 
   res.send(results);
 });
+
+// Create list
+router.post(
+  '/',
+  validateRequest([
+    body('name').isLength({ min: 3, max: 50 }).trim().escape(),
+    body('iconName').isLength({ min: 3, max: 50 }).trim().escape(),
+  ]),
+  (req, res) => {
+    console.log('posted', req);
+
+    res.sendStatus(200);
+  },
+);
 
 export default router;
