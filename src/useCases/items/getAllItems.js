33edd1c98f547fb9aa2ee1cb5b0c9f2c7ae20ce8
beakerandjake <beakerandjake@users.noreply.github.com>
2022-09-23 11:23:48 -0700

    ensure list exists

--- src/useCases/items/getAllItems.js
+++ src/useCases/items/getAllItems.js
@@ -1,12 +1,17 @@
-import { itemRepository } from '../../repositories/index.js';
+import { itemRepository, listRepository } from '../../repositories/index.js';
 import { itemModel } from '../../models/index.js';
+import { NotFoundError } from '../../errors/NotFound.js';
 
 /**
  * Returns all of the items in the list.
  * @returns {Array}
  */
 export const getAllItems = (listId) => {
-  // todo does list exist?
-  const result = itemRepository.getAllItems(listId);
-  return result.map(itemModel);
+  if (!listRepository.existsWithId(listId)) {
+    throw new NotFoundError('List does not exist');
+  }
+
+  return itemRepository
+    .getAllItems(listId)
+    .map(itemModel);
 };
