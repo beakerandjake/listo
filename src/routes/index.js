4963bc4c1ee83415930851e0d2b4fd8bc46d37bd
beakerandjake <beakerandjake@users.noreply.github.com>
2022-09-24 09:02:28 -0700

    add items route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
-// import items from './items.js';
+import items from './items.js';
 import lists from './lists.js';
 
 const router = express.Router();
 
 // add routes here.
-// router.use('/items', items);
+router.use('/items', items);
 router.use('/lists', lists);
 
 export default router;
