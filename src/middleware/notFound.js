b3b330214e4a5b0243b07b2c5b7c372d77d21a10
beakerandjake <beakerandjake@users.noreply.github.com>
2022-09-24 09:16:14 -0700

    log 404s

--- src/middleware/notFound.js
+++ src/middleware/notFound.js
@@ -1,4 +1,9 @@
+import { logger } from '../logger.js';
+
 /**
  * Express middleware which always returns a 404 status.
  */
-export const notFound = () => (req, res) => res.sendStatus(404);
+export const notFound = () => (req, res) => {
+  logger.http('404 unknown route %s', req.url);
+  res.sendStatus(404);
+};
