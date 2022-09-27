9a3c18d85cf94bd7b106a7f6b5e498ef29b6413c
beakerandjake <beakerandjake@users.noreply.github.com>
2022-09-27 08:26:53 -0700

    wip custom error boundary

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,16 +1,40 @@
 import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
+import { Logo } from './Logo';
 
-export const ErrorBoundary = ({ fallbackComponent, children }) => {
+/**
+ * Informs the user that something went wrong.
+ */
+const ErrorDisplay = () => {
+  return (
+    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col justify-center items-center text-center gap-2">
+      <div className="mt-5 mb-10">
+        <Logo />
+      </div>
+      <h1 className="font-bold text-2xl text-slate-900">Unexpected Error</h1>
+      <h3 className="text-lg">Something went wrong, please try again later.</h3>
+      <a
+        href="/"
+        className="text-blue-500 hover:text-blue-700 transition duration-300 ease-in-out"
+      >
+        Return Home
+      </a>
+    </div>
+  );
+};
+
+/**
+ * Component which catches errors in child components.
+ * @param {Object} props - The props.
+ * @param {React.ReactNode} props.children - The child elements to render.
+ */
+export const ErrorBoundary = ({ children }) => {
   const logError = (error, info) => {
     console.error('unexpected error', error, info);
   };
 
   return (
-    <ReactErrorBoundary
-      fallbackComponent={fallbackComponent}
-      onError={logError}
-    >
+    <ReactErrorBoundary FallbackComponent={ErrorDisplay} onError={logError}>
       {children}
-    </ReactErrorBoundary>
+    // </ReactErrorBoundary>
   );
 };
