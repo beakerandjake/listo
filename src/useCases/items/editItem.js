c9ec470d3878aefaa3f4da282f90710d46c1872e
beakerandjake <beakerandjake@users.noreply.github.com>
2022-09-28 11:50:41 -0700

    return edited item

--- src/useCases/items/editItem.js
+++ src/useCases/items/editItem.js
@@ -1,12 +1,13 @@
 import { NotFoundError } from '../../errors/NotFound.js';
 import { logger } from '../../logger.js';
-import { editItemModel } from '../../models/editItem.js';
+import { itemModel, editItemModel } from '../../models/index.js';
 import { itemRepository } from '../../repositories/index.js';
 
 /**
  * Applies edits to an item.
  * @param {number} id - The id of item to edit.
  * @param {object} edits - The edits to apply to the item.
+ * @returns {object}
  */
 export const editItem = (id, edits) => {
   logger.info('editing item: %s with changes: %s', id, edits);
@@ -20,4 +21,6 @@ export const editItem = (id, edits) => {
   }
 
   logger.info('edited item: %s', itemId);
+
+  return itemModel(itemRepository.getItem(itemId));
 };
