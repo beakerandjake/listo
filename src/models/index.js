70793496a9bfb359f676f82af68c5692f6f6e6bc
beakerandjake <beakerandjake@users.noreply.github.com>
2022-09-29 09:53:44 -0700

    add model

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,7 @@ import { itemModel, itemIdModel } from './item.js';
 import { listModel, listIdModel } from './list.js';
 import { deleteItemsModel } from './deleteItems.js';
 import { editItemModel } from './editItem.js';
+import { editItemsModel } from './editItems.js';
 
 export {
   createListRequestModel,
@@ -15,4 +16,5 @@ export {
   listIdModel,
   itemIdModel,
   editItemModel,
+  editItemsModel,
 };
