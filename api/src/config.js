a138e0770db5349ae9d19ad2f625b77b0140a02b
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-03 09:46:27 -0700

    better defaults when running locally

--- api/src/config.js
+++ api/src/config.js
@@ -5,14 +5,14 @@ const version = process.env.VERSION || '0.1.0';
 // When running in a container this is the port of the container
 // That gets exposed, however it is not necessarily the port that the
 // hose uses for communication.
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4001;
 
 export default {
   environment,
   port,
   version,
   sqlite: {
-    dbLocation: process.env.SQLITE_LOCATION || '/var/lib/listo/items.db',
+    dbLocation: process.env.SQLITE_LOCATION || './items.db',
   },
   swaggerJSDoc: {
     definition: {
