da326b80ba5f27f5322e4179b18b7cac918b300f
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-05 14:19:32 -0700

    formatting

--- client/src/routes/List/listLoader.js
+++ client/src/routes/List/listLoader.js
@@ -7,13 +7,13 @@ import { itemApi, listApi } from 'api';
  * @param {number} props.id - The id of the list.
  */
 export const listLoader = async ({ params }) => {
-    const result = await Promise.all([
-      listApi.getList(params.id),
-      itemApi.getItems(params.id),
-    ]);
+  const result = await Promise.all([
+    listApi.getList(params.id),
+    itemApi.getItems(params.id),
+  ]);
 
-    return {
-      list: result[0],
-      items: result[1],
-    };
+  return {
+    list: result[0],
+    items: result[1],
+  };
 };
