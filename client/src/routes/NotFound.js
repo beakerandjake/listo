1a1dfcd3d088cf5aa9ee1fd31d3dcd65b9e4a2d0
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-05 12:22:03 -0700

    re-throw error if not 404

--- client/src/routes/NotFound.js
+++ client/src/routes/NotFound.js
@@ -1,6 +1,16 @@
 import { PageHeader } from 'components/PageHeader';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 
 export function NotFound() {
+  const error = useRouteError();
+
+  // If the error is coming from a react-router-dom loader
+  // check to see if the error status is actually a 404.
+  // If it's not a 404, re-throw the error so a different error handler can handle it.
+  if (isRouteErrorResponse(error) && error.status !== 404) {
+    throw error;
+  }
+
   return (
     <>
       <PageHeader name="Not Found" />
