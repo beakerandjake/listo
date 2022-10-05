1788c9eb04e04b2354491eae7d5d57d8a68b4049
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-05 12:22:34 -0700

    Add error component

---
+++ client/src/routes/Error.js
@@ -0,0 +1,22 @@
+const { Logo } = require('components/Logo');
+
+/**
+ * Error page which informs the user something went wrong.
+ */
+export const Error = () => {
+  return (
+    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col justify-center items-center text-center gap-2">
+      <div className="mt-5 mb-10">
+        <Logo />
+      </div>
+      <h1 className="font-bold text-2xl text-slate-900">Unexpected Error</h1>
+      <h3 className="text-lg">Something went wrong, please try again later.</h3>
+      <a
+        href="/"
+        className="text-blue-500 hover:text-blue-700 transition duration-300 ease-in-out"
+      >
+        Return Home
+      </a>
+    </div>
+  );
+};
---
+++ client/src/routes/index.js
@@ -0,0 +1,7 @@
+import { CreateNewList } from './CreateNewList';
+import { Dashboard } from './Dashboard';
+import { Error } from './Error';
+import { List } from './List';
+import { NotFound } from './NotFound';
+
+export { CreateNewList, Dashboard, Error, List, NotFound };
