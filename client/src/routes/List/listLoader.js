f41438c5cafe62c80b62943b3d28394cff9f76ad
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-05 12:54:32 -0700

    update comments

--- client/src/routes/List/listLoader.js
+++ client/src/routes/List/listLoader.js
@@ -4,7 +4,7 @@ import { itemApi, listApi } from 'api';
  * Loader function used by react-router to load the data needed to render
  * the List component.
  * @param {Object} params
- * @param {number} props.id - The id of the list.
+ * @param {number} params.id - The id of the list.
  */
 export const listLoader = async ({ params }) => {
   const result = await Promise.all([
