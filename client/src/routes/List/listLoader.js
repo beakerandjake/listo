998fbd70c9748f48b83fddbd576d47acddc6c2bb
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-06 10:21:13 -0700

    update loader

--- client/src/routes/List/listLoader.js
+++ client/src/routes/List/listLoader.js
@@ -7,13 +7,10 @@ import { itemApi, listApi } from 'api';
  * @param {number} props.id - The id of the list.
  */
 export const listLoader = async ({ params }) => {
-  const result = await Promise.all([
+  const [list, items] = await Promise.all([
     listApi.getList(params.id),
     itemApi.getItems(params.id),
   ]);
 
-  return {
-    list: result[0],
-    items: result[1],
-  };
+  return { list, items };
 };
