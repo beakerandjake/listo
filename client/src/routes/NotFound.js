9e49435ca7d3d072eb205cfbd0cb2c2ddc845966
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-06 13:31:35 -0700

    update display

--- client/src/routes/NotFound.js
+++ client/src/routes/NotFound.js
@@ -1,4 +1,5 @@
-import { PageHeader } from 'components/PageHeader';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFaceSpiralEyes } from '@fortawesome/pro-light-svg-icons';
 import { useRouteError } from 'react-router-dom';
 
 export function NotFound() {
@@ -12,11 +13,15 @@ export function NotFound() {
   }
 
   return (
-    <>
-      <PageHeader name="Not Found" />
-      <div className="py-4">
-        <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+    <div className="flex justify-center items-center mt-5">
+      <div className="text-center">
+        <FontAwesomeIcon
+          icon={faFaceSpiralEyes}
+          size="10x"
+          className="text-gray-600"
+        />
+        <h3 className="mt-3 text-xl font-medium text-gray-500">Not Found</h3>
       </div>
-    </>
+    </div>
   );
 }
