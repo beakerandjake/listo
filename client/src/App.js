6294d2a1605e3915764e9ce0e08b06289660963a
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-06 11:56:00 -0700

    first pass using updated react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,58 @@
 import React, { useEffect, useState } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { NotFound } from 'routes/NotFound';
-import { ResponsiveLayout } from 'components/ResponsiveLayout';
-import { Sidebar } from 'components/Navigation/Sidebar';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import { useErrorHandler } from 'react-error-boundary';
 import { listApi } from './api';
-import { CreateNewList } from 'routes/CreateNewList';
-import { List } from 'routes/List';
-import { Dashboard } from 'routes/Dashboard';
+import { ResponsiveLayout } from 'components/ResponsiveLayout';
+import { Sidebar } from 'components/Navigation/Sidebar';
+import { CreateNewList, Dashboard, Error, List, NotFound } from 'routes';
+import { listLoader } from 'routes/List';
 
 function App() {
   const [lists, setLists] = useState(null);
   const handleError = useErrorHandler();
 
   useEffect(() => {
-    listApi.getLists()
-      .then(setLists)
-      .catch(handleError);
+    listApi.getLists().then(setLists).catch(handleError);
   }, [handleError]);
 
   if (!lists) {
     return <div>Loading...</div>;
   }
 
-  return (
-    <BrowserRouter>
-      <ResponsiveLayout sidebar={<Sidebar items={lists} />}>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="lists">
-            <Route path=":id" element={<List />} />
-            <Route path="create" element={<CreateNewList />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </ResponsiveLayout>
-    </BrowserRouter>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <ResponsiveLayout sidebar={<Sidebar items={lists} />}>
+          <Outlet />
+        </ResponsiveLayout>
+      ),
+      children: [
+        {
+          path: '',
+          element: <Dashboard />,
+        },
+        {
+          path: 'lists/:id',
+          element: <List />,
+          loader: listLoader,
+          errorElement: <NotFound />,
+        },
+        { path: 'lists/create', element: <CreateNewList /> },
+        {
+          path: 'coolguy',
+          element: <NotFound />,
+        },
+        {
+          path: '*',
+          element: <NotFound />,
+        },
+      ],
+      errorElement: <Error />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
