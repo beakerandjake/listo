d29faa767d85a95b968653f744e827fa0d64ab22
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-07 10:14:47 -0700

    memoize context value

--- client/src/routes/Root.js
+++ client/src/routes/Root.js
@@ -1,6 +1,6 @@
 import { ResponsiveLayout } from 'components/ResponsiveLayout';
 import { SidebarContext } from 'context/SidebarContext';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Outlet, useRouteLoaderData } from 'react-router-dom';
 
 export const routeId = 'root';
@@ -11,7 +11,10 @@ export const routeId = 'root';
  * as well as rendering the current route via outlet.
  */
 export const Root = () => {
+  // the react-router loader for this route will provide the initial value of the lists.
   const loaderData = useRouteLoaderData(routeId);
+  // maintain internal state of lists, so we can edit itemCounts
+  // as the user makes changes to the items in a list without having to re-query an api
   const [lists, setLists] = useState(loaderData);
 
   // Any time that the loader provides new data, be sure to update our lists
@@ -20,17 +23,19 @@ export const Root = () => {
     setLists(loaderData);
   }, [loaderData]);
 
-  /**
-   * Updates the active item count of the specified list.
-   * @param {number} id - The id of the list to edit
-   * @param {number} itemCount - The number of active items in the list
-   */
-  const updateItemCount = (id, itemCount) => {
-    setLists(lists.map((x) => (x.id === id ? { ...x, itemCount } : x)));
-  };
+  // memoize the context value to prevent re-rendering.
+  // only update this context value whenever our lits change.
+  const contextValue = useMemo(() => {
+    // allow consumers of the context to update the item count of a list whenever items are edited.
+    const updateItemCount = (id, itemCount) => {
+      setLists(lists.map((x) => (x.id === id ? { ...x, itemCount } : x)));
+    };
+
+    return { lists, updateItemCount };
+  }, [lists]);
 
   return (
-    <SidebarContext.Provider value={{ lists, updateItemCount }}>
+    <SidebarContext.Provider value={contextValue}>
       <ResponsiveLayout>
         <Outlet />
       </ResponsiveLayout>
