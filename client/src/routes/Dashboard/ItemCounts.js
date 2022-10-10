5201c3b4cbab24b13f0ea488bc2ed21ec8c4b160
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-10 12:01:38 -0700

    use variants

--- client/src/routes/Dashboard/ItemCounts.js
+++ client/src/routes/Dashboard/ItemCounts.js
@@ -1,8 +1,8 @@
 import { StatCard } from './StatCard';
 
 /**
- * 
- * @param {object} props 
+ *
+ * @param {object} props
  * @param {number} total - The total number of items
  * @param {number} total - The total number of items
  * @param {number} total - The total number of items
@@ -15,16 +15,11 @@ export const ItemCounts = ({
   overdue = 0,
 }) => {
   return (
-    <div>
-      <h3 className="text-lg font-medium leading-6 text-gray-900">
-        Item Stats
-      </h3>
-      <dl className="mt-5 grid grid-cols-2 gap-5 sm:grid-cols-4 ">
-        <StatCard name="Total Items" stat={total} />
-        <StatCard name="Active Items" stat={active} />
-        <StatCard name="Completed Items" stat={completed} />
-        <StatCard name="Overdue Items" stat={overdue} />
-      </dl>
-    </div>
+    <dl className="mt-5 grid grid-cols-2 gap-3 lg:grid-cols-4">
+      <StatCard name="Total Items" stat={total} />
+      <StatCard name="Active Items" stat={active} variant="secondary" />
+      <StatCard name="Completed Items" stat={completed} variant="primary" />
+      <StatCard name="Overdue Items" stat={overdue} variant="danger" />
+    </dl>
   );
 };
