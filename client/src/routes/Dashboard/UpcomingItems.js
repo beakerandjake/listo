cfa554161225abbbeb433a3b4196575facd9a998
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-10 14:39:17 -0700

    dispatch updates to sidebar when items change

--- client/src/routes/Dashboard/UpcomingItems.js
+++ client/src/routes/Dashboard/UpcomingItems.js
@@ -3,8 +3,13 @@ import {
   ListItemsDispatchContext,
   listItemsReducer,
 } from 'context/ListItemsContext';
-import { useReducer } from 'react';
+import {
+  sidebarItemsActions,
+  useSidebarItemsDispatch,
+} from 'context/SidebarItemsContext';
+import { useEffect, useReducer } from 'react';
 import { GroupedItemsDisplay } from 'routes/List/GroupedItemsDisplay';
+import { itemSortingFields, sortingDirections } from 'services/sorting';
 
 /**
  * Shows items across all lists which are due soon.
@@ -12,6 +17,33 @@ import { GroupedItemsDisplay } from 'routes/List/GroupedItemsDisplay';
  */
 export const UpcomingItems = ({ items: initialItems }) => {
   const [items, listItemsDispatch] = useReducer(listItemsReducer, initialItems);
+  const sidebarItemsDispatch = useSidebarItemsDispatch();
+
+  // any time the items change, dispatch updates to the sidebar items context.
+  useEffect(() => {
+    // group the items by list id and calculate the active item count.
+    const groups = items.reduce((acc, value) => {
+      const key = `list_${value.listId}`;
+
+      if (!acc[key]) {
+        acc[key] = { id: value.listId, itemCount: 0 };
+      }
+
+      if (!value.completed) {
+        acc[key].itemCount += 1;
+      }
+
+      return acc;
+    }, {});
+
+    // dispatch a sidebar update for each list
+    Object.values(groups).forEach((group) => {
+      sidebarItemsDispatch({
+        type: sidebarItemsActions.update,
+        ...group,
+      });
+    });
+  }, [items, sidebarItemsDispatch]);
 
   return (
     <div>
@@ -23,8 +55,8 @@ export const UpcomingItems = ({ items: initialItems }) => {
       <ListItemsContext.Provider value={items}>
         <ListItemsDispatchContext.Provider value={listItemsDispatch}>
           <GroupedItemsDisplay
-            sortingDirection="asc"
-            sortingKey="dueDate"
+            sortingDirection={sortingDirections.asc}
+            sortingKey={itemSortingFields.dueDate}
             items={items}
           />
         </ListItemsDispatchContext.Provider>
