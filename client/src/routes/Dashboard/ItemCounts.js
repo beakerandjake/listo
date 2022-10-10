70d5a3630d7a4ea843fe75f8e57f9c1763fea07b
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-10 12:23:25 -0700

    update grid

--- client/src/routes/Dashboard/ItemCounts.js
+++ client/src/routes/Dashboard/ItemCounts.js
@@ -39,7 +39,7 @@ export const ItemCounts = ({
   );
 
   return (
-    <dl className="mt-5 grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-4">
+    <dl className="mt-5 grid grid-cols-2 gap-3 md:grid-cols-4">
       <StatCard name="Total Items" stat={total} />
       <StatCard
         name="Active"
