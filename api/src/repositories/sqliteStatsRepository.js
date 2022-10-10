eb4d0a586bee11d077f40a675259fceba819173f
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-10 12:08:40 -0700

    consider whether due date has passed

--- api/src/repositories/sqliteStatsRepository.js
+++ api/src/repositories/sqliteStatsRepository.js
@@ -14,7 +14,12 @@ const getItemCounts = () => {
         COUNT(*) as total,
         SUM(CASE WHEN i.completedDate IS NULL THEN 1 ELSE 0 END) AS active,
         SUM(CASE WHEN i.completedDate IS NULL THEN 0 ELSE 1 END) AS completed,
-        SUM(CASE WHEN i.completedDate IS NULL AND i.dueDate IS NOT NULL THEN 1 ELSE 0 end) AS overdue
+        SUM(
+          CASE WHEN i.completedDate IS NULL 
+          AND i.dueDate IS NOT NULL 
+          AND date(i.dueDate) < date('now') 
+            THEN 1 ELSE 0 end
+        ) AS overdue
       FROM items i
       WHERE i.deletedDate IS NULL;
     `)
