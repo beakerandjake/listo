86dd3ad13a59fd8838b76ac5f3ad14b91da68c5a
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-10 12:56:48 -0700

    rename use case

--- api/src/useCases/dashboard/getTodaySummary.js
+++ api/src/useCases/dashboard/getTodaySummary.js
@@ -3,10 +3,10 @@ import { createItemCountStats } from '../../models/itemCountStats.js';
 import { sqliteDashboardRepository } from '../../repositories/index.js';
 
 /**
- * Returns items due today.
+ * Returns items due today across all lists.
  * @returns {object}
  */
-export const getItemCounts = () => {
+export const getItemsDueToday = () => {
   logger.info('getting item count stats');
 
   const result = sqliteDashboardRepository.getItemCounts();
