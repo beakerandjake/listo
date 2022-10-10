b841879c7231f079949562b248cb9ae980b12b36
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-10 13:47:17 -0700

    use component

--- client/src/routes/Dashboard/Dashboard.js
+++ client/src/routes/Dashboard/Dashboard.js
@@ -2,18 +2,28 @@ import { PageHeader } from 'components/PageHeader';
 import { Suspense } from 'react';
 import { Await, useLoaderData } from 'react-router-dom';
 import { ItemCounts } from './ItemCounts';
+import { UpcomingItems } from './UpcomingItems';
 
 export function Dashboard(props) {
   const loaderData = useLoaderData();
 
   return (
-    <div className="flex-1 flex flex-col gap-2">
-      <PageHeader name="Dashboard"/>
-      <Suspense fallback={<p>Loading Dashboard...</p>}>
-        <Await resolve={loaderData.itemCounts}>
-          {(itemCounts) => <ItemCounts {...itemCounts} />}
-        </Await>
-      </Suspense>
-    </div>
+    <>
+      <PageHeader name="Dashboard" />
+      <div className="flex-1 flex flex-col gap-7">
+        {/* Item Counts */}
+        <Suspense fallback={<p>Loading Dashboard...</p>}>
+          <Await resolve={loaderData.itemCounts}>
+            {(itemCounts) => <ItemCounts {...itemCounts} />}
+          </Await>
+        </Suspense>
+        {/* Item Due Today */}
+        <Suspense fallback={<p>Loading due today...</p>}>
+          <Await resolve={loaderData.itemsDueToday}>
+            {(itemsDueToday) => <UpcomingItems items={itemsDueToday} />}
+          </Await>
+        </Suspense>
+      </div>
+    </>
   );
 }
