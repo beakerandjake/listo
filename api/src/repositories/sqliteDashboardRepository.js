b24a5c54dffc23943d4ff4955526d8b201eb99c3
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-11 15:23:29 -0700

    getOverDueItems repo method

--- api/src/repositories/sqliteDashboardRepository.js
+++ api/src/repositories/sqliteDashboardRepository.js
@@ -58,7 +58,34 @@ const getItemsByDueDateRange = (startDate, endDate) => {
   return result;
 };
 
+/**
+ * Returns all of the active, non-deleted items which:
+ *  1. Do not have a completed date set
+ *  2. Have a due date which comes before the specified date.
+ * @param {string} dueDate - ISO8601 formatted date string.
+ * @returns {object[]}
+ */
+const getOverdueItems = (dueDate) => {
+  logger.verbose('querying for all items overdue after: %s', dueDate);
+
+  const result = getDb()
+    .prepare(`
+      SELECT ${getItemFieldsForSelectStatement()}
+      FROM items
+      WHERE 
+        deletedDate IS NULL AND 
+        completedDate IS NULL AND 
+        completedDate < ?
+    `)
+    .all(dueDate);
+
+  logger.verbose('queried %d item(s) overdue after: %s', result.length, dueDate);
+
+  return result;
+};
+
 export default {
   getItemCounts,
   getItemsByDueDateRange,
+  getOverdueItems,
 };
