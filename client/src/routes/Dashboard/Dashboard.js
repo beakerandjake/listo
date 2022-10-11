01e8faa6a1601d4da573b3e9fd3d2d27046a905a
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-11 12:19:27 -0700

    update item counts

--- client/src/routes/Dashboard/Dashboard.js
+++ client/src/routes/Dashboard/Dashboard.js
@@ -1,29 +1,54 @@
-import { PageHeader } from 'components/PageHeader';
-import { Suspense } from 'react';
+import { Suspense, useCallback, useEffect, useState } from 'react';
+import { useErrorHandler } from 'react-error-boundary';
 import { Await, useLoaderData } from 'react-router-dom';
+import { dashboardApi } from 'api';
+import { PageHeader } from 'components/PageHeader';
 import { ItemCounts } from './ItemCounts';
+import { SectionHeading } from './SectionHeading';
 import { UpcomingItems } from './UpcomingItems';
 
-export function Dashboard(props) {
+export const Dashboard = () => {
+  const handleError = useErrorHandler();
   const loaderData = useLoaderData();
+  
+  // because the item count stats are reloaded whenever
+  // the user edits items, handle this portion outside of the loader.
+  const [itemCounts, setItemCounts] = useState(null);
+
+  // query api for the latest item count statistics.
+  const updateItemCounts = useCallback(() => {
+    dashboardApi
+      .getItemCounts()
+      .then((result) => setItemCounts(result))
+      .catch(handleError);
+  }, [handleError]);
+
+  // get item counts on load.
+  useEffect(() => {
+    updateItemCounts();
+  }, [updateItemCounts]);
 
   return (
     <>
       <PageHeader name="Dashboard" />
       <div className="flex-1 flex flex-col gap-7">
         {/* Item Counts */}
-        <Suspense fallback={<p>Loading Dashboard...</p>}>
-          <Await resolve={loaderData.itemCounts}>
-            {(itemCounts) => <ItemCounts {...itemCounts} />}
-          </Await>
-        </Suspense>
+        <ItemCounts {...itemCounts} />
         {/* Item Due Today */}
-        <Suspense fallback={<p>Loading due today...</p>}>
-          <Await resolve={loaderData.itemsDueToday}>
-            {(itemsDueToday) => <UpcomingItems items={itemsDueToday} />}
-          </Await>
-        </Suspense>
+        <div>
+          <SectionHeading title="Items Due Today" />
+          <Suspense fallback={<p>Loading due today...</p>}>
+            <Await resolve={loaderData.itemsDueToday}>
+              {(itemsDueToday) => (
+                <UpcomingItems
+                  items={itemsDueToday}
+                  onItemChange={updateItemCounts}
+                />
+              )}
+            </Await>
+          </Suspense>
+        </div>
       </div>
     </>
   );
-}
+};
