e7b823474ba0e97b69a78d8fcca17c0a7a374545
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-11 16:05:21 -0700

    first pass get all items with filter

--- api/src/useCases/items/getAllItems.js
+++ api/src/useCases/items/getAllItems.js
@@ -1,21 +1,37 @@
 import { startOfToday, endOfToday } from 'date-fns';
 import { logger } from '../../logger.js';
-import { itemModel } from '../../models/index.js';
+import { getItemsFilter, itemModel } from '../../models/index.js';
 import { itemRepository } from '../../repositories/index.js';
+import { filters } from '../../models/getItemsFilter.js';
+import { ApplicationError } from '../../errors/ApplicationError.js';
 
 /**
- * Returns items due today across all lists.
+ * Returns items across all lists based on a filter criteria.
  * @returns {object}
  */
-export const getAllItems = () => {
-  logger.info('getting items due today');
+export const getAllItems = (filter) => {
+  logger.info('getting items according to filter: %s', filter);
 
-  const result = itemRepository.getItemsByDueDateRange(
-    startOfToday().toISOString(),
-    endOfToday().toISOString(),
-  );
+  const filterModel = getItemsFilter(filter);
+  let result;
 
-  logger.info('got: %d item(s) due today', result.length);
+  switch (filterModel) {
+    case filters.overdue:
+      result = itemRepository.getOverdueItems(
+        startOfToday().toISOString(),
+      );
+      break;
+    case filters.dueToday:
+      result = itemRepository.getItemsByDueDateRange(
+        startOfToday().toISOString(),
+        endOfToday().toISOString(),
+      );
+      break;
+    default:
+      throw new ApplicationError(`getAllItems got unimplemented filter: ${filter}`);
+  }
+
+  logger.info('got: %d item(s) according to filter: %s', result.length, filter);
 
   return result.map(itemModel);
 };
