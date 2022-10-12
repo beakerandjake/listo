4c184b770b215e889247f277d0179badfae37bbd
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-12 10:18:22 -0700

    change border

--- client/src/components/CollapsibleCard.js
+++ client/src/components/CollapsibleCard.js
@@ -12,11 +12,7 @@ import cx from 'classnames';
  */
 const CardHeader = ({ title, open }) => {
   return (
-    <div
-      className={cx('px-4 py-5 sm:px-6 cursor-pointer', {
-        'border-b border-gray-300': open,
-      })}
-    >
+    <div className="px-4 py-5 sm:px-6 cursor-pointer">
       <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
         <div className="ml-4 mt-2">
           <h3 className="text-lg font-medium leading-6 text-gray-900">
@@ -38,7 +34,7 @@ const CardHeader = ({ title, open }) => {
  * Card with a collapsible body.
  * @param {object} props
  * @param {string} props.title - The title of the card.
- * @param {boolean} props.defaultOpenState - Should the card default to being opened?
+ * @param {boolean} props.defaultOpen - Should the card default to being opened?
  * @param {ReactNode} props.children - The child element to render.
  */
 export const CollapsibleCard = ({ title, defaultOpen = false, children }) => {
@@ -46,11 +42,14 @@ export const CollapsibleCard = ({ title, defaultOpen = false, children }) => {
     <Disclosure
       as="div"
       defaultOpen={defaultOpen}
-      className="overflow-hidden rounded-lg bg-white shadow border border-gray-300"
+      className="overflow-hidden rounded-lg bg-white shadow border border-gray-300 divide-y divide-y-300"
     >
       {({ open }) => (
         <>
-          <Disclosure.Button title={open ? 'Close Panel' : 'Open Panel'} className="w-full">
+          <Disclosure.Button
+            title={open ? 'Close Panel' : 'Open Panel'}
+            className="w-full"
+          >
             <CardHeader title={title} open={open}></CardHeader>
           </Disclosure.Button>
           <Disclosure.Panel className="bg-gray-50 px-4 py-5 sm:p-6">
