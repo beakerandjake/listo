5d99bc8f5fd47c6d90d2921c5e353aa89de9a207
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-13 13:54:16 -0700

    call api

--- client/src/api/statsApi.js
+++ client/src/api/statsApi.js
@@ -16,8 +16,23 @@ const getItemCounts = async () => {
   return await response.json();
 };
 
+/**
+ * Returns the average item completion time stats.
+ * @returns {Promise<object>}
+ **/
+const getAverageItemCompletionTime = async () => {
+  const response = await fetch(`${baseUrl}/avg-completion-time`);
+
+  if (!response.ok) {
+    throw new ApiError(response.statusText, response.status);
+  }
+
+  return await response.json();
+};
+
 const api = {
   getItemCounts,
+  getAverageItemCompletionTime,
 };
 
 export default api;
