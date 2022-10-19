47507bde1fe2bff641902f22b1c0eea6c1ff4391
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-19 14:52:55 -0700

    fix exports

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,5 +1,7 @@
 import { Card } from './Card';
 import { CardHeading } from './CardHeading';
+import { CardHeader } from './CardHeader';
 import { CardContent } from './CardContent';
+import { CollapsibleCard } from './CollapsibleCard';
 
-export { Card, CardHeading, CardContent };
+export { Card, CardHeading, CardHeader, CardContent, CollapsibleCard };
