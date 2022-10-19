caaedda6d876b96ab79c22cab4604f27f6f1010d
beakerandjake <beakerandjake@users.noreply.github.com>
2022-10-19 11:43:44 -0700

    add collapsible card

---
+++ client/src/components/Card/CollapsibleCard.js
@@ -0,0 +1,45 @@
+import { useCallback, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/pro-regular-svg-icons';
+import cx from 'classnames';
+import { Card } from './Card';
+import { CardHeader } from './CardHeader';
+import { Fade } from 'components/Transition';
+
+/**
+ * Card with a collapsible body.
+ * @param {object} props
+ * @param {boolean} props.defaultOpen - Initial collapsed state of the card.
+ * @param {ReactNode} props.header - Content of the Cards Header.
+ * @param {ReactNode} props.children - Content of the.
+ */
+export const CollapsibleCard = ({
+  defaultOpen,
+  header,
+  children,
+  ...props
+}) => {
+  const [open, setOpen] = useState(!!defaultOpen);
+  const toggle = useCallback(() => setOpen((prev) => !prev), []);
+
+  return (
+    <Card {...props}>
+      {/* Header */}
+      <CardHeader onClick={toggle}>
+        <div className="flex items-center justify-between">
+          <div>{header}</div>
+          <FontAwesomeIcon
+            icon={faChevronDown}
+            className={cx('transition-transform', { 'rotate-180': open })}
+          />
+        </div>
+      </CardHeader>
+      {/* Body */}
+      {open && (
+        <Fade in={open} appear>
+          <div>{children}</div>
+        </Fade>
+      )}
+    </Card>
+  );
+};
