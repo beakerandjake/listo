20a37b2e1e9a63ac434d898bbc70e47ad0578993
beakerandjake <beakerandjake@users.noreply.github.com>
2022-11-10 13:38:08 -0700

    use new component

--- client/src/components/CreateList/CreateListDrawer.js
+++ client/src/components/CreateList/CreateListDrawer.js
@@ -1,31 +1,53 @@
-import { faTimes } from '@fortawesome/pro-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { RadioGroup } from '@headlessui/react';
 import { Button } from 'components/Button';
 import { Drawer } from 'components/Drawer';
-import { IconButton } from 'components/IconButton';
 import {
   MenuFooter,
   MenuHeader,
   MenuTitle,
   ScrollableMenuContent,
 } from 'components/Menu';
-import { useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { icons } from 'services/iconLibrary';
-import cx from 'classnames';
+import { SelectListIcon } from './SelectListIcon';
+
+const DEFAULT_MODEL = {
+  name: '',
+  icon: icons[0],
+};
 
 const validationConstants = {
   nameMaxLength: 50,
   nameMinLength: 3,
 };
 
+const validateModel = (name, icon) => {
+  if (
+    !name ||
+    name.length < validationConstants.nameMinLength ||
+    name.length > validationConstants.nameMaxLength
+  ) {
+    return false;
+  }
+
+  if (!icon?.iconName) {
+    return false;
+  }
+
+  return true;
+};
+
 export const CreateListDrawer = ({ open = false, onClose = () => {} }) => {
-  // min 3 max 50
-  // todo react-hook-form
+  const [name, setName] = useState(DEFAULT_MODEL.name);
+  const [icon, setIcon] = useState(DEFAULT_MODEL.icon);
+  const isValid = useMemo(() => validateModel(name, icon), [name, icon]);
 
-  const [name, setName] = useState('');
-  const [icon, setIcon] = useState(null);
-
+  // Reset back to default values whenever the drawer closes.
+  useEffect(() => {
+    if (!open) {
+      setName(DEFAULT_MODEL.name);
+      setIcon(DEFAULT_MODEL.icon);
+    }
+  }, [open]);
 
   return (
     <Drawer
@@ -35,14 +57,14 @@ export const CreateListDrawer = ({ open = false, onClose = () => {} }) => {
       size="full"
       contentClassName="max-w-md"
     >
-      <MenuHeader className="md:p-5">
+      <MenuHeader className="!p-4">
         <MenuTitle>Create New List</MenuTitle>
       </MenuHeader>
-      <ScrollableMenuContent className="flex flex-col py-6 px-4 sm:px-6 gap-3 bg-gray-50">
+      <ScrollableMenuContent className="flex flex-col py-6 px-4 sm:px-6 gap-8 bg-gray-50">
         {/* List Name */}
         <div>
           <label className="block text-md font-medium text-gray-700">
-            List Title
+            Title
           </label>
           <div className="mt-1">
             <input
@@ -57,35 +79,14 @@ export const CreateListDrawer = ({ open = false, onClose = () => {} }) => {
             />
           </div>
         </div>
-        {/* Icon */}
-        <RadioGroup value={icon} onChange={setIcon}>
-          <RadioGroup.Label className="block text-md font-medium text-gray-700">
-            Icon
-          </RadioGroup.Label>
-          <div className="grid grid-cols-3 gap-3 sm:grid-cols-6 mt-2 select-none">
-            {icons.map((x) => (
-              <RadioGroup.Option key={x.iconName} value={x.iconName}>
-                {({ active, checked }) => (
-                  <div
-                    className={cx(
-                      active ? 'ring-2 ring-offset-2 ring-green-600' : '',
-                      checked
-                        ? 'bg-green-700 border-transparent text-white hover:bg-green-800'
-                        : 'bg-white border-gray-200 text-gray-900 hover:bg-gray-50',
-                      'cursor-pointer focus:outline-none border rounded-md py-3 px-3 flex items-center justify-center'
-                    )}
-                  >
-                    <FontAwesomeIcon icon={x} size="lg" fixedWidth />
-                  </div>
-                )}
-              </RadioGroup.Option>
-            ))}
-          </div>
-        </RadioGroup>
+        {/* List Icon */}
+        <SelectListIcon value={icon} onChange={setIcon} icons={icons} />
       </ScrollableMenuContent>
       <MenuFooter className="flex items-center gap-2 flex-shrink-0 justify-end">
         <Button onClick={onClose}>Cancel</Button>
-        <Button variant="success">Create</Button>
+        <Button variant="success" disabled={!isValid}>
+          Create
+        </Button>
       </MenuFooter>
     </Drawer>
   );
