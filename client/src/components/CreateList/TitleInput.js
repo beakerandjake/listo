9ee78c50229f5589ad690ed4198a280c7a53deea
beakerandjake <beakerandjake@users.noreply.github.com>
2022-11-14 12:48:45 -0700

    title input accepts an error state

--- client/src/components/CreateList/TitleInput.js
+++ client/src/components/CreateList/TitleInput.js
@@ -1,3 +1,6 @@
+import cx from 'classnames';
+import { faExclamationCircle } from '@fortawesome/pro-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { listValidationConstants } from './CreateListDrawer';
 
 /**
@@ -6,23 +9,34 @@ import { listValidationConstants } from './CreateListDrawer';
  * @param {string} props.value - The value
  * @param {function} props.onChange - Callback invoked when name value changes.
  */
-export const TitleInput = ({ value, onChange }) => {
+export const TitleInput = ({ error, value, onChange }) => {
   return (
     <div>
       <label className="block text-md font-medium text-gray-700">Title</label>
-      <div className="mt-1">
+      <div className="relative mt-1">
         <input
           value={value}
           onChange={(e) => onChange(e.target.value)}
           type="text"
-          className="block w-full rounded-md border-gray-300 shadow-sm keyboard-only-focus-ring sm:text-sm"
+          className={cx(
+            error 
+              ? 'border-red-300 text-red-900 placeholder-red-300 focus:border-red-500 focus:outline-none focus:ring-red-500'
+              : 'border-gray-300 keyboard-only-focus-ring',
+            'block w-full rounded-md  shadow-sm  sm:text-sm'
+          )}
           autoComplete="off"
           enterKeyHint="done"
           minLength={listValidationConstants.nameMinLength}
           maxLength={listValidationConstants.nameMaxLength}
           autoFocus={true}
         />
+        {!!error && (
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-red-500">
+            <FontAwesomeIcon icon={faExclamationCircle} />
+          </div>
+        )}
       </div>
+      {!!error && <p className="mt-2 text-md text-red-600">{error}</p>}
     </div>
   );
 };
