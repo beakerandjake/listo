8c3f6916ab739f45234f07571fae220a406d6465
beakerandjake <beakerandjake@users.noreply.github.com>
2022-11-25 05:22:02 -0700

    wip using mock apis

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
-import listApi from './listApi';
+import realListApi from './listApi';
 import itemApi from './itemApi';
 import statsApi from './statsApi';
+import mockApi from './mock';
 
 // TODO swap based on config
 
@@ -10,5 +11,7 @@ class ApiError extends Error {
     this.statusCode = statusCode;
   }
 }
+
+const listApi = { ...realListApi, getLists: mockApi.getLists };
 
 export { listApi, itemApi, statsApi, ApiError };
