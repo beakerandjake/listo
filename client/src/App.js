5fd0e11c9965167251cdce299e2bcf7f893069d5
beakerandjake <beakerandjake@users.noreply.github.com>
2022-11-28 16:44:29 -0700

    add dynamic router

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,48 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import { listApi } from './api';
 import { Dashboard, Error, List, NotFound } from 'routes';
 import { listLoader } from 'routes/List';
 import { LoadingSpinner } from 'components/LoadingSpinner';
 import { Root, routeId as rootRouteId } from 'routes/Root';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    id: rootRouteId,
-    element: <Root />,
-    loader: async () => await listApi.getLists(),
-    children: [
-      {
-        index: true,
-        element: <Dashboard />,
-      },
-      {
-        path: 'lists/:listId',
-        element: <List />,
-        loader: listLoader,
-        errorElement: <NotFound />,
-      },
-      {
-        path: '*',
-        element: <NotFound />,
-      },
-    ],
-    errorElement: <Error />,
-  },
-]);
+const ROUTES = [
+  [
+    {
+      path: '/',
+      id: rootRouteId,
+      element: <Root />,
+      loader: async () => await listApi.getLists(),
+      children: [
+        {
+          index: true,
+          element: <Dashboard />,
+        },
+        {
+          path: 'lists/:listId',
+          element: <List />,
+          loader: listLoader,
+          errorElement: <NotFound />,
+        },
+        {
+          path: '*',
+          element: <NotFound />,
+        },
+      ],
+      errorElement: <Error />,
+    },
+  ],
+];
+
+// Some hosting providers don't support browser router, when deploying to
+// choose router based on current configuration to support for these providers.
+const router = process.env.REACT_APP_USE_HASH_ROUTER
+  ? createHashRouter(ROUTES)
+  : createBrowserRouter(ROUTES);
 
 function App() {
   return (
